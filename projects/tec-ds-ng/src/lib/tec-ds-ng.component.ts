c27f9eee3ae895b7a47a3e3307d10ba48d1f16aa
Humberto Avila <humberto.avila@tec.mx>
2023-12-21 14:30:59 -0600

    Create library and configuration

---
+++ projects/tec-ds-ng/src/lib/tec-ds-ng.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-tec-ds-ng',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      tec-ds-ng works!
+    </p>
+  `,
+  styles: ``
+})
+export class TecDsNgComponent {
+
+}
---
+++ projects/tec-ds-ng/src/public-api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of tec-ds-ng
+ */
+
+export * from './lib/tec-ds-ng.service';
+export * from './lib/tec-ds-ng.component';
