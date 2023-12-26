125d941c2d9d5814623cef0c99aa660e2c82e068
Humberto Avila <humberto.avila@tec.mx>
2023-12-26 14:31:52 -0600

    Assets en libreria

---
+++ src/app/app.module.ts
@@ -0,0 +1,16 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+
+import { AppComponent } from "./app.component";
+ 
+
+@NgModule({
+    declarations: [],
+    imports: [
+        BrowserModule,
+    ],
+    providers: [],
+    bootstrap: []
+})
+
+export class AppModule { }
