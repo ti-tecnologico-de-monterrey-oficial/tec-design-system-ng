3c0546b1c2a57f738e1affaa0689df053699c688
Humberto Avila <humberto.avila@tec.mx>
2023-12-27 15:38:14 -0600

    create library

---
+++ projects/ds-ng/src/lib/ds-ng.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'bmb-ds-ng',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      ds-ng works!
+    </p>
+  `,
+  styles: ``
+})
+export class DsNgComponent {
+
+}
