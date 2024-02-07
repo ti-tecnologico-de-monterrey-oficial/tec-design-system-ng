735619c1462326e35824a59367578e81b696e895
LizTec5 <t-lizvillegas@itesm.mx>
2024-02-07 11:00:56 -0600

    adding storybook

--- projects/ds-ng/src/stories/button/button.stories.ts
+++ projects/ds-ng/src/stories/button/button.stories.ts
@@ -1,5 +1,4 @@
 import { Meta, moduleMetadata, Story } from '@storybook/angular';
-import { Source } from '@storybook/blocks';
 import { BmbButtonDirective } from '../../lib/directives/button.directive';
 import { BmbIconComponent } from '../../lib/components/bmb-icon/bmb-icon.component';
 
@@ -13,7 +12,7 @@ export default {
   argTypes: {
     appearance: {
       name: 'Appearance',
-      options: ['primary', 'secondary', 'simple'],
+      options: ['primary', 'secondary', 'alternative', 'destructive'],
       control: { type: 'select' },
     },
     icon: {
@@ -22,10 +21,24 @@ export default {
       description:
         'Nombre del ícono a utilizar. Por favor, utiliza los íconos de Material. https://fonts.google.com/icons',
     },
+    size: {
+      name: 'Size',
+      control: 'radio',
+      options: ['small', 'large'],
+    },
     iconPosition: {
       name: 'Icon Position',
       control: 'radio',
       options: ['left', 'right'],
+    },
+    iconCase: {
+      name: 'Icon Case',
+      control: { type: 'boolean' },
+    },
+    device: {
+      name: 'Device',
+      control: 'radio',
+      options: ['mobile', 'desktop'],
     },
     text: {
       name: 'Texto del boton',
@@ -49,7 +62,7 @@ export default {
 
 const Template: Story<any> = (args: any) => ({
   template: `
-    <button bmbButton [appearance]="appearance" [icon]="icon" [iconPosition]="iconPosition">{{ text }}</button>
+    <button bmbButton [appearance]="appearance" [device]="device" [iconCase]="iconCase" [size]="size" [icon]="icon" [iconPosition]="iconPosition">{{ text }}</button>
   `,
   props: args,
 });
@@ -59,18 +72,20 @@ Primary.args = {
   appearance: 'primary',
   icon: 'home',
   iconPosition: 'left',
-  text: 'VER TODOS LOS SERVICIOS',
+  text: 'Primary',
+  size: 'small',
+  device: '',
+  iconCase: false,
 };
 Primary.parameters = {
   docs: {
     source: {
-      code: `<button bmbButton type="button" appearance="primary">Primary</button>  <button
+      code: `<button
       bmbButton
       icon="chevron_left"
-      iconPosition="left"
       appearance="primary"
     >
-      VER TODOS LOS SERVICIOS
+      Primary
     </button>
       `,
     },
@@ -82,28 +97,53 @@ Secondary.args = {
   appearance: 'secondary',
   icon: 'chevron_left',
   iconPosition: 'right',
-  text: 'VER TODOS LOS SERVICIOS',
+  text: 'Secondary',
+  size: 'large',
+  device: 'desktop',
+  iconCase: true,
 };
 Secondary.parameters = {
   docs: {
     source: {
-      code: `<button bmbButton type="button" appearance="secondary">Secondary</button> <button bmbButton icon="chevron_right" iconPosition="right" appearance="secondary">VER TODOS LOS SERVICIOS</button>
+      code: `<button bmbButton icon="chevron_right" iconPosition="right" size="large" device="desktop" [iconCase]="true" appearance="secondary">Secondary</button>
       `,
     },
   },
 };
 
-export const Simple = Template.bind({});
-Simple.args = {
-  appearance: 'simple',
+export const Alternative = Template.bind({});
+Alternative.args = {
+  appearance: 'alternative',
   icon: 'chevron_right',
   iconPosition: 'left',
-  text: 'VER TODOS LOS SERVICIOS',
+  text: 'Alternative',
+  size: 'small',
+  device: '',
+  iconCase: '',
 };
-Simple.parameters = {
+Alternative.parameters = {
   docs: {
     source: {
-      code: `<button bmbButton type="button" appearance="simple">Secondary</button> <button bmbButton icon="chevron_right" iconPosition="right" appearance="simple">VER TODOS LOS SERVICIOS</button>
+      code: `<button bmbButton icon="chevron_right" iconPosition="right" appearance="alternative">Alternative</button>
+      `,
+    },
+  },
+};
+
+export const Destructive = Template.bind({});
+Destructive.args = {
+  appearance: 'destructive',
+  icon: 'chevron_right',
+  iconPosition: 'left',
+  text: 'Destructive',
+  size: 'small',
+  device: '',
+  iconCase: '',
+};
+Destructive.parameters = {
+  docs: {
+    source: {
+      code: `<button bmbButton icon="chevron_right" iconPosition="right" appearance="destructive">Alternative</button>
       `,
     },
   },
