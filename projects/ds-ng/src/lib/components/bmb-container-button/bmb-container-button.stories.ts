0b65b8a951225cd479d57fb7f11114a9f7e537fa
LizTec5 <t-lizvillegas@itesm.mx>
2024-04-04 11:23:19 -0600

    changes on storybook

--- projects/ds-ng/src/lib/components/bmb-container-button/bmb-container-button.stories.ts
+++ projects/ds-ng/src/lib/components/bmb-container-button/bmb-container-button.stories.ts
@@ -4,7 +4,7 @@ import { BmbContainerButtonComponent } from './bmb-container-button.component';
 import { BmbContainerComponent } from '../bmb-container/bmb-container.component';
 
 export default {
-  title: 'Container Button',
+  title: 'Macro Components/Container Button',
   component: BmbContainerButtonComponent,
   decorators: [
     moduleMetadata({
