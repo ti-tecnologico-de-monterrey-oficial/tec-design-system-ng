4413deeba3476e83c8fa18e7c334442c33f9a55e
thezeeck <t-juansanchez@itesm.mx>
2024-04-08 18:11:14 -0600

    fix: User summary component story updated

--- projects/ds-ng/src/lib/components/bmb-user-summary/bmb-user-summary.component.ts
+++ projects/ds-ng/src/lib/components/bmb-user-summary/bmb-user-summary.component.ts
@@ -1,11 +1,12 @@
 import { Component,Input, ViewEncapsulation, Output, EventEmitter } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BmbButtonDirective } from '../../directives/button.directive';
+import { BmbUserImageComponent } from '../bmb-user-image/bmb-user-image.component';
 
 @Component({
-  selector: 'bmb-user-summary',
+  selector: 'Macro Componentes/User Summary',
   standalone: true,
-  imports: [CommonModule, BmbButtonDirective],
+  imports: [CommonModule, BmbButtonDirective, BmbUserImageComponent],
   templateUrl: './bmb-user-summary.component.html',
   styleUrl: './bmb-user-summary.component.scss',
   encapsulation: ViewEncapsulation.None,
@@ -14,7 +15,7 @@ export class BmbUserSummaryComponent {
   @Input() isProfile: boolean = false;
   @Input() name: string = '';
   @Input() id: string = '';
-  @Input() profilePic: string = '';
+  @Input() image: string = '';
   @Output() onClick: EventEmitter<void> = new EventEmitter<void>();
 
   handleClick(): void {
