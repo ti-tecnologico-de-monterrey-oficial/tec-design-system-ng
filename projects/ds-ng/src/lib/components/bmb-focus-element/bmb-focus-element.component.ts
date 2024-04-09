226904a636f299a1b68e1815a6dbbe7f9d4789b9
LizTec5 <t-lizvillegas@itesm.mx>
2024-04-09 10:13:02 -0600

    Feat: Angular/Ionic - Focus element/ highlight

---
+++ projects/ds-ng/src/lib/components/bmb-focus-element/bmb-focus-element.component.ts
@@ -0,0 +1,60 @@
+import {
+  Component,
+  Input,
+  ChangeDetectionStrategy,
+  ElementRef,
+  Renderer2,
+  AfterViewInit,
+  ViewEncapsulation,
+} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BmbIconComponent } from '../bmb-icon/bmb-icon.component';
+
+@Component({
+  selector: 'bmb-focus-element',
+  styleUrl: './bmb-focus-element.component.scss',
+  templateUrl: './bmb-focus-element.component.html',
+  standalone: true,
+  imports: [CommonModule, BmbIconComponent],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  encapsulation: ViewEncapsulation.None,
+})
+export class BmbFocusElementComponent implements AfterViewInit {
+  @Input() appearance: string = '';
+  @Input() title: string = '';
+  @Input() description: string = '';
+  @Input() icon: string = '';
+  @Input() grouped: boolean = false;
+  @Input() horizontal: boolean = false;
+  @Input() target: string = '';
+  @Input() link: string = '';
+
+  constructor(private el: ElementRef, private renderer: Renderer2) {}
+
+  ngAfterViewInit() {
+    if (this.grouped) {
+      const parentElement = this.el.nativeElement.parentElement;
+
+      let wrapperDiv = parentElement.querySelector(
+        '.bmb_interactive_icon-grouped'
+      );
+      if (!wrapperDiv) {
+        wrapperDiv = this.renderer.createElement('div');
+        this.renderer.addClass(wrapperDiv, 'bmb_interactive_icon-grouped');
+        this.renderer.insertBefore(parentElement, wrapperDiv, null);
+      }
+
+      this.renderer.appendChild(wrapperDiv, this.el.nativeElement);
+    }
+  }
+
+  getClasses(): string[] {
+    const classes: string[] = ['bmb_interactive_icon'];
+
+    if (this.appearance) {
+      classes.push('bmb_interactive_icon-' + this.appearance);
+    }
+
+    return classes;
+  }
+}
---
+++ projects/ds-ng/src/lib/components/bmb-focus-element/bmb-focus-element.stories.ts
@@ -0,0 +1,143 @@
+import { Meta, StoryObj, moduleMetadata } from '@storybook/angular';
+import { BmbIconComponent } from '../bmb-icon/bmb-icon.component';
+import { BmbFocusElementComponent } from './bmb-focus-element.component';
+
+export default {
+  title: 'Micro Componentes/Focus Element',
+  component: BmbFocusElementComponent,
+  decorators: [
+    moduleMetadata({
+      imports: [BmbIconComponent],
+    }),
+  ],
+  parameters: {
+    docs: {
+      description: {
+        component: `
+Below is an example of how you can use this component in TypeScript:
+
+\`\`\`typescript
+import { BmbFocusElementComponent } from '@ti-tecnologico-de-monterrey-oficial/ds-ng';
+@Component({
+  selector: 'component',
+  standalone: true,
+  imports: [ BmbFocusElementComponent ],
+  templateUrl: './component.html',
+  styleUrl: './component.scss',
+})
+\`\`\`
+
+Below is an example of how you can use this component in HTML:
+        `,
+      },
+    },
+  },
+  argTypes: {
+    title: {
+      name: 'Title',
+      control: {
+        type: 'text',
+      },
+      description: 'The title of the interactive icon.',
+      table: {
+        category: 'Properties',
+        type: { summary: 'string' },
+      },
+    },
+    description: {
+      name: 'Description',
+      control: {
+        type: 'text',
+      },
+      description: 'The description of the interactive icon.',
+      table: {
+        category: 'Properties',
+        type: { summary: 'string' },
+      },
+    },
+    grouped: {
+      name: 'Grouped',
+      control: { type: 'boolean' },
+      description:
+        'When set to true, it groups multiple interactive icons into a parent element. By default, it is false, and you do not need to explicitly set it. The badge should always have a parent element.',
+      table: {
+        category: 'Properties',
+        defaultValue: { summary: 'false' },
+        type: { summary: 'boolean' },
+      },
+    },
+    icon: {
+      name: 'Icon',
+      control: { type: 'text' },
+      description:
+        'Name of the icon to use. Please use Material icons: https://fonts.google.com/icons. Do not use the image property if you want to use an icon.',
+      table: {
+        category: 'Properties',
+        type: { summary: 'string' },
+      },
+    },
+    appearance: {
+      name: 'Appearance',
+      control: {
+        type: 'radio',
+      },
+      options: ['red', 'blue', 'green', 'yellow', 'purple'],
+      description:
+        'The appearance of the interactive icon, affecting its visual style.',
+      table: {
+        category: 'Properties',
+        defaultValue: { summary: 'red' },
+        type: { summary: 'string' },
+      },
+    },
+    link: {
+      name: 'Link',
+      control: {
+        type: 'text',
+      },
+      description: 'The link for redirection to another page.',
+      table: {
+        category: 'Events',
+        type: { summary: 'string' },
+      },
+    },
+    target: {
+      name: 'Target',
+      control: {
+        type: 'radio',
+      },
+      options: ['_blank', '_self', '_parent', '_top'],
+      description:
+        'The target property for the link. Refer to https://developer.mozilla.org/en-US/docs/Web/HTML/Element/a for more information.',
+      table: {
+        category: 'Events',
+        type: { summary: 'string' },
+      },
+    },
+    horizontal: {
+      name: 'Horizontal',
+      control: { type: 'boolean' },
+      description:
+        'This property is effective when you want to include a description with a horizontal orientation.',
+      table: {
+        category: 'Properties',
+        defaultValue: { summary: 'false' },
+        type: { summary: 'boolean' },
+      },
+    },
+  },
+  args: {
+    title: 'Canvas',
+    description: 'Short Description',
+    appearance: 'red',
+    icon: 'face',
+    grouped: false,
+    target: '_blank',
+    link: 'https://www.youtube.com/',
+    horizontal: false,
+  },
+} as Meta<typeof BmbFocusElementComponent>;
+
+type Story = StoryObj<BmbFocusElementComponent>;
+
+export const Default: Story = {};
