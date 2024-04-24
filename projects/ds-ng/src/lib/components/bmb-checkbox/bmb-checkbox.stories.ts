dd82e8bd3c0a3c7ca8721a446418d927e3a624de
LizTec57 <t-lizvillegas@itesm.mx>
2024-04-24 09:36:15 -0600

    Feat: checkbox

--- projects/ds-ng/src/lib/components/bmb-checkbox/bmb-checkbox.stories.ts
+++ projects/ds-ng/src/lib/components/bmb-checkbox/bmb-checkbox.stories.ts
@@ -33,43 +33,145 @@ Below is an example of how you can use this component in HTML:
     },
   },
   argTypes: {
-    title: {
-      name: 'Title',
-      control: {
-        type: 'text',
-      },
-      description: 'The title of the focus element.',
-      table: {
-        category: 'Properties',
-        type: { summary: 'string' },
-      },
-    },
-    number: {
-      name: 'Number',
-      control: {
-        type: 'text',
-      },
-      description: 'The number of the focus element.',
-      table: {
-        category: 'Properties',
-        type: { summary: 'string' },
-      },
-    },
-    icon: {
-      name: 'Icon',
+    id: {
+      name: 'Id',
       control: { type: 'text' },
       description:
-        'Name of the icon to use. Please use Material icons: https://fonts.google.com/icons. Do not use the number property if you want to use an icon.',
+        'The unique identifier for the checkbox component. It is used to link the label with the checkbox input element through the "for" attribute, enhancing accessibility and usability.',
       table: {
         category: 'Properties',
         type: { summary: 'string' },
+      },
+    },
+    checked: {
+      name: 'Checked',
+      control: { type: 'boolean' },
+      description:
+        'Determines whether the checkbox is checked or not. Setting this property to true checks the checkbox, and false unchecks it.',
+      table: {
+        category: 'Properties',
+        defaultValue: { summary: 'false' },
+        type: { summary: 'boolean' },
+      },
+    },
+    disabled: {
+      name: 'Disabled',
+      control: { type: 'boolean' },
+      description:
+        'If set to true, disables the checkbox input, making it non-interactive and unclickable. This is useful for conditions where user interaction should be restricted.',
+      table: {
+        category: 'Properties',
+        defaultValue: { summary: 'false' },
+        type: { summary: 'boolean' },
+      },
+    },
+    indeterminate: {
+      name: 'Indeterminate',
+      control: { type: 'boolean' },
+      description:
+        'Sets the checkbox to an indeterminate state, which is typically used to represent a mixed state in complex forms like tree views or nested lists. Note: This does not affect the checked property and is purely visual.',
+      table: {
+        category: 'Properties',
+        defaultValue: { summary: 'false' },
+        type: { summary: 'boolean' },
+      },
+    },
+    value: {
+      name: 'Value',
+      control: { type: 'text' },
+      description:
+        'The value that will be submitted with the form if the checkbox is checked. This is the value that will be sent to the server or handled on form submission.',
+      table: {
+        category: 'Properties',
+        type: { summary: 'string' },
+      },
+    },
+    name: {
+      name: 'Name',
+      control: { type: 'text' },
+      description:
+        'The name of the checkbox, which is used to identify the form data after it’s submitted. Multiple checkboxes can share the same name to create a group where multiple items can be selected.',
+      table: {
+        category: 'Properties',
+        type: { summary: 'string' },
+      },
+    },
+    label: {
+      name: 'Label',
+      control: { type: 'text' },
+      description:
+        'The text label associated with the checkbox. It is displayed next to the checkbox and helps users understand the context of what the checkbox represents.',
+      table: {
+        category: 'Properties',
+        type: { summary: 'string' },
+      },
+    },
+    ariaDescribedby: {
+      name: 'Aria Described by',
+      control: { type: 'text' },
+      description:
+        'Provides additional descriptive text for the checkbox, enhancing accessibility by linking the checkbox to a descriptive element by ID.',
+      table: {
+        category: 'Properties',
+        type: { summary: 'string' },
+      },
+    },
+    ariaLabel: {
+      name: 'Aria Label',
+      control: { type: 'text' },
+      description:
+        'Defines a string that labels the checkbox for accessibility purposes, which can be used when a visible label text is not present.',
+      table: {
+        category: 'Properties',
+        type: { summary: 'string' },
+      },
+    },
+    ariaLabelledby: {
+      name: 'Aria Labelled by',
+      control: { type: 'text' },
+      description:
+        'Identifies the element(s) that labels the checkbox for accessibility purposes, providing a reference to the IDs of the elements that serve as the checkbox label.',
+      table: {
+        category: 'Properties',
+        type: { summary: 'string' },
+      },
+    },
+    labelPosition: {
+      name: 'Label Position',
+      control: { type: 'text' },
+      description:
+        'Specifies the position of the label relative to the checkbox. Can be set to "before" or "after", indicating whether the label appears to the left or right of the checkbox.',
+      table: {
+        category: 'Properties',
+        type: { summary: 'string' },
+      },
+    },
+    change: {
+      name: 'Change',
+      description:
+        'An event that is emitted when the state of the checkbox changes, such as when it is checked or unchecked. This can be used to trigger functions or actions based on the checkbox’s state change.',
+      table: {
+        category: 'Events',
+        type: { summary: '(change)="handleChange($event)"' },
       },
     },
   },
+
   args: {
-    title: 'Title',
-    icon: 'close',
-    number: '',
+    id: 'checkbox1',
+    checked: '',
+    disabled: '',
+    indeterminate: '',
+    value: '',
+    name: '',
+    label: 'Contrato profesor cátedra Biología marina CCM.pdf',
+    ariaDescribedby: '',
+    ariaLabel: '',
+    ariaLabelledby: '',
+    labelPosition: 'before',
+    change: () => {
+      window.alert('Checkbox clicked');
+    },
   },
 } as Meta<typeof BmbCheckboxComponent>;
 
