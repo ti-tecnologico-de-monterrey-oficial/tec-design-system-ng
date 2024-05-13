accd99e69cd9117a397b3e7fc750694c95e0de1f
thezeeck <165101597+t-juansanchez@users.noreply.github.com>
2024-05-13 15:34:10 -0600

    feat: layout system added

---
+++ projects/ds-ng/src/lib/directives/bmb-layout/bmb-layout-item.directive.ts
@@ -0,0 +1,48 @@
+import {
+  Directive,
+  HostBinding,
+  Input,
+} from '@angular/core';
+
+interface IMargin {
+  sm: number;
+  lg?: number;
+}
+
+@Directive({
+  selector: '[bmbLayoutItem]',
+  standalone: true,
+})
+export class BmbLayoutItemDirective {
+  @Input() colSm: number = 0;
+  @Input() colLg: number = 0;
+  @Input() marginLeft: IMargin | null = null;
+  @Input() marginRight: IMargin | null = null;
+  @Input() colGrow: number = 0;
+
+  constructor() {}
+
+  @HostBinding('class') get elementClass(): string[] {
+    const classes = [];
+
+    if (this.colSm) classes.push(`bmb_col-sm-${this.colSm}`);
+    if (this.colLg) classes.push(`bmb_col-lg-${this.colLg}`);
+    if (this.marginLeft) {
+      classes.push(`bmb_space-sm-left-${this.marginLeft?.sm}`);
+
+      if (this.marginLeft.lg)
+        classes.push(`bmb_space-lg-left-${this.marginLeft?.lg}`);
+    }
+    if (this.marginRight) {
+      classes.push(`bmb_space-sm-right-${this.marginRight?.sm}`);
+
+      if (this.marginRight.lg)
+        classes.push(`bmb_space-lg-right-${this.marginRight?.lg}`);
+    }
+    return classes;
+  }
+
+  @HostBinding('style.flex-grow') get flexGrow(): string {
+    return this.colGrow.toString();
+  }
+}
