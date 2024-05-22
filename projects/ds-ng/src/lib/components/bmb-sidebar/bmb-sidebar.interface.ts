4b9db9e09b32bd6df57fe2cc409e0a441c936f5e
LizTec57 <t-lizvillegas@itesm.mx>
2024-05-22 12:14:49 -0600

    Fix: Sanitizacion de sidebar

--- projects/ds-ng/src/lib/components/bmb-sidebar/bmb-sidebar.interface.ts
+++ projects/ds-ng/src/lib/components/bmb-sidebar/bmb-sidebar.interface.ts
@@ -1,5 +1,10 @@
-export interface SidebarElements {
+export interface SidebarElement {
+  id: number;
   icon: string;
   title: string;
   link: string;
+}
+
+export interface SidebarElements {
+  child: SidebarElement[];
 }
--- projects/ds-ng/src/lib/components/bmb-sidebar/bmb-sidebar.stories.ts
+++ projects/ds-ng/src/lib/components/bmb-sidebar/bmb-sidebar.stories.ts
@@ -1,33 +1,95 @@
-import type { Meta, StoryObj } from '@storybook/angular';
+import { Meta, StoryObj, moduleMetadata } from '@storybook/angular';
 import { BmbSidebarComponent } from './bmb-sidebar.component';
+import { BmbIconComponent } from '../bmb-icon/bmb-icon.component';
+import { CommonModule } from '@angular/common';
+import { RouterTestingModule } from '@angular/router/testing';
 
 export default {
-  title: 'Micro Componentes/Sidebar',
+  title: 'Macro Componentes/Sidebar',
   component: BmbSidebarComponent,
+  decorators: [
+    moduleMetadata({
+      imports: [CommonModule, BmbIconComponent, RouterTestingModule],
+    }),
+  ],
+  parameters: {
+    docs: {
+      description: {
+        component: `
+The \`BmbSidebarComponent\` is a versatile sidebar component that can be used to display a list of navigation items. Below is an example of how to use this component in TypeScript:
+
+\`\`\`typescript
+import { BmbSidebarComponent } from '@ti-tecnologico-de-monterrey-oficial/ds-ng';
+
+@Component({
+  selector: 'app-component',
+  standalone: true,
+  imports: [ BmbSidebarComponent ],
+  templateUrl: './app-component.html',
+  styleUrls: ['./app-component.scss'],
+})
+export class AppComponent {
+  sidebarElements = [
+    {child: [{ id: 1, icon: 'task', title: 'Add Signers', link: '#' }]},
+    {child: [{ id: 2, icon: 'account_box', title: 'Manage Users', link: '/home' }]}
+  ];
+}
+\`\`\`
+
+Below is an example of how to use this component in HTML:
+
+\`\`\`html
+<bmb-sidebar [elements]="sidebarElements"></bmb-sidebar>
+\`\`\`
+        `,
+      },
+    },
+  },
   argTypes: {
     elements: {
       name: 'Elements',
       control: {
-        type: '',
+        type: 'object',
       },
       description:
-        'Array of elements to show in the sidebar, every element must contain the icon, the title and the link',
+        'An array of sidebar elements, each containing a list of child elements.',
       table: {
-        type: { summary: 'string' },
+        type: {
+          summary:
+            'Array<{ child: { id: number; icon: string; title: string; link: string; }[] }>',
+        },
       },
     },
   },
   args: {
     elements: [
       {
-        icon: 'apps',
-        title: 'Titulo 1',
-        link: '#',
+        child: [
+          {
+            id: 1,
+            icon: 'assignment_add',
+            title: 'Select Documents',
+            link: '#',
+          },
+          { id: 2, icon: 'task', title: 'Add Signers', link: '#' },
+          { id: 3, icon: 'note_alt', title: 'Create Envelope', link: '#' },
+          {
+            id: 4,
+            icon: 'checklist_rtl',
+            title: 'File Status',
+            link: '#',
+          },
+        ],
       },
       {
-        icon: 'apps',
-        title: 'Titulo 2',
-        link: '#',
+        child: [
+          {
+            id: 5,
+            icon: 'account_box',
+            title: 'Manage Users',
+            link: '/home',
+          },
+        ],
       },
     ],
   },
