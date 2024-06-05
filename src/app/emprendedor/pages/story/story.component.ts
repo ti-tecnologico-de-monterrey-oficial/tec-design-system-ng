2be3487bccfff7a1cf60d1d01e1e97c3106c4dae
LizTec57 <t-lizvillegas@itesm.mx>
2024-06-05 14:34:17 -0600

    Sanitizar PARTICULARIDAD Table component

---
+++ src/app/emprendedor/pages/story/story.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-story',
+  standalone: true,
+  imports: [],
+  templateUrl: './story.component.html',
+  styleUrl: './story.component.scss',
+})
+export default class StoryComponent {}
