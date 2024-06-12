5ba5727c2413824cf90d455872843e954ac30092
Hazel Humberto Ávila Soto <humberto.avila@tec.mx>
2024-06-12 17:53:05 -0600

    feat: Se agrega dropdown

---
+++ projects/ds-ng/src/lib/components/bmb-dropdown/bmb-dropdown.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { BmbDropdownComponent } from './bmb-dropdown.component';
+
+describe('BmbDropdownComponent', () => {
+  let component: BmbDropdownComponent;
+  let fixture: ComponentFixture<BmbDropdownComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [BmbDropdownComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(BmbDropdownComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ projects/ds-ng/src/lib/components/bmb-dropdown/bmb-dropdown.component.ts
@@ -0,0 +1,172 @@
+import { CommonModule } from '@angular/common';
+import { AfterViewInit, ChangeDetectionStrategy, ChangeDetectorRef, Component, ElementRef, EventEmitter, HostListener, Input, OnChanges, OnInit, Output, SimpleChanges, ViewChild, ViewEncapsulation, forwardRef } from '@angular/core';
+import { ControlValueAccessor, FormControl, NG_VALIDATORS, NG_VALUE_ACCESSOR, NgModel, ReactiveFormsModule } from '@angular/forms';
+import { BmbIconComponent } from '../bmb-icon/bmb-icon.component';
+import { BmbLoaderComponent } from '../bmb-loader/bmb-loader.component';
+
+@Component({
+  selector: 'bmb-dropdown',
+  standalone: true,
+  imports: [CommonModule, BmbIconComponent, BmbLoaderComponent, ReactiveFormsModule],
+  templateUrl: './bmb-dropdown.component.html',
+  styleUrl: './bmb-dropdown.component.scss',
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => BmbDropdownComponent),
+      multi: true,
+    },
+    {
+      provide: NG_VALIDATORS,
+      useExisting: forwardRef(() => BmbDropdownComponent),
+      multi: true,
+    },
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  encapsulation: ViewEncapsulation.None,
+})
+export class BmbDropdownComponent implements AfterViewInit, ControlValueAccessor, OnChanges, OnInit {
+
+  @Input() required?: boolean;
+  @Input() showIcon?: boolean = false;
+  @Input() placeholder?: string = '';
+  @Input() icon?: string = '';
+  @Input() options?: string[] = [];
+  @Input() helperText?: string = '';
+  @Input() formControl?: FormControl | undefined;
+  @Input() disabled?: boolean = false;
+  @Input() label?: string;
+
+  @Output() onValueChange: EventEmitter<string> = new EventEmitter<string>();
+
+  @ViewChild('inputWrapper', { read: ElementRef, static: false })
+  inputWrapper: ElementRef | undefined;
+
+  @ViewChild('filterDropdown') filterField: ElementRef | null = null;
+
+
+  @ViewChild('input', { static: false })
+  input: NgModel | undefined;
+
+  private childNodes: any = null;
+
+  @HostListener('document:click', ['$event'])
+  onClick(event: MouseEvent) {
+    if (!this.childNodes?.contains(event.target)) {
+      this.openSelect = false;
+      this.isFocus = this.isFocus == false ? false : true
+    }
+  }
+
+  isFocus: boolean = false;
+  selectedIndexOption?: number;
+  selectedOption?: any;
+
+  uid: string = Date.now().toString(36) + (Math.floor(Math.random() * 90) + 10);
+  filterControl= new FormControl();
+  filteredData: string[] = [];
+  
+  value: string = '';
+  openSelect: boolean = false
+  // isDialogOpen: boolean = false;
+
+  constructor(private cdr: ChangeDetectorRef, private elementRef: ElementRef,){}
+
+  ngOnInit(): void {
+    if(this.formControl === undefined){
+      this.formControl = new FormControl()
+    }
+  }
+  
+  ngAfterViewInit(): void {
+    this.childNodes = this.elementRef.nativeElement;
+    // console.log("this. disabled", this.disabled, "this.formContorl", this.formControl)
+    if (this.disabled && this.formControl) {
+      this.formControl.disable();
+    } else if(this.disabled && !this.formControl){
+      this.input?.disabled
+    }
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (this.formControl && changes?.['disabled'] !== undefined) {
+      if (this.formControl.disabled !== changes?.['disabled']?.currentValue) {
+        changes?.['disabled']?.currentValue
+          ? this.formControl.disable()
+          : this.formControl.enable();
+      }
+    }
+    // this.formControl!.value = this.selectedOption
+
+
+  }
+
+  handleItemClick(event: string, index: any): void {
+    this.onValueChange.emit(event);
+    this.selectedIndexOption = index
+    this.selectedOption = event
+    this.isFocus = !this.isFocus
+    this.openSelect = false;
+    if(this.formControl){
+      this.formControl.setValue(event)
+      console.log("this.FORMA VALUE", this.formControl)
+      // this.formControl.
+    }
+    if (this.filterField?.nativeElement) {
+      this.filterField.nativeElement.value = event;
+    }
+  }
+
+  openDialog(){
+    this.openSelect = !this.openSelect
+    this.isFocus = !this.isFocus
+  }
+
+  onParentClick(){
+    this.isFocus = !this.isFocus
+  }
+
+  onKeyDown(event: KeyboardEvent){
+    if (['Enter', ' ', 'ArrowDown', 'Down', 'ArrowUp', 'Up'].indexOf(event.key) > -1) {
+      if (!this.openSelect) {
+        this.openDialog()
+      }
+    
+      if (!this.options!.length) {
+        event.preventDefault();
+        return;
+      }
+    }
+
+  }
+
+  public onChangeFn = (_: any) => {};
+ 
+  public onTouchedFn = () => {};
+ 
+  public registerOnChange(fn: any): void {
+    this.onChangeFn = fn;
+  }
+ 
+  public registerOnTouched(fn: any): void {
+    this.onTouchedFn = fn;
+  }
+ 
+  public writeValue(obj: any): void {
+    this.value = obj;
+  }
+ 
+  public onTouched() {
+    this.onTouchedFn();
+  }
+ 
+  public onChange() {
+    this.onChangeFn(this.value);
+  }
+
+  // setValidation(validation: any) {
+  //   this.isValid = validation;
+  // }
+
+
+}
