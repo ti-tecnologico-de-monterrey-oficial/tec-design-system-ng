898f71309ddea09c78df0ecfba0ae1826c7be813
thezeeck <165101597+t-juansanchez@users.noreply.github.com>
2024-06-14 16:11:15 -0600

    Fix: API updated

--- projects/ds-ng/src/lib/components/bmb-dropdown/bmb-dropdown.component.ts
+++ projects/ds-ng/src/lib/components/bmb-dropdown/bmb-dropdown.component.ts
@@ -1,6 +1,29 @@
 import { CommonModule } from '@angular/common';
-import { AfterViewInit, ChangeDetectionStrategy, ChangeDetectorRef, Component, ElementRef, EventEmitter, HostListener, Input, OnChanges, OnInit, Output, SimpleChanges, ViewChild, ViewEncapsulation, forwardRef } from '@angular/core';
-import { ControlValueAccessor, FormControl, NG_VALIDATORS, NG_VALUE_ACCESSOR, NgModel, ReactiveFormsModule } from '@angular/forms';
+import {
+  AfterViewInit,
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  ElementRef,
+  EventEmitter,
+  HostListener,
+  Input,
+  OnChanges,
+  OnInit,
+  Output,
+  SimpleChanges,
+  ViewChild,
+  ViewEncapsulation,
+  forwardRef,
+} from '@angular/core';
+import {
+  ControlValueAccessor,
+  FormControl,
+  NG_VALIDATORS,
+  NG_VALUE_ACCESSOR,
+  NgModel,
+  ReactiveFormsModule,
+} from '@angular/forms';
 import { BmbIconComponent } from '../bmb-icon/bmb-icon.component';
 import { BmbLoaderComponent } from '../bmb-loader/bmb-loader.component';
 import { BmbTagComponent } from '../bmb-tags/bmb-tags.component';
@@ -10,8 +33,8 @@ import { BmbTagComponent } from '../bmb-tags/bmb-tags.component';
   standalone: true,
   imports: [
     CommonModule,
-    BmbIconComponent, 
-    BmbLoaderComponent, 
+    BmbIconComponent,
+    BmbLoaderComponent,
     ReactiveFormsModule,
     BmbTagComponent,
   ],
@@ -32,8 +55,9 @@ import { BmbTagComponent } from '../bmb-tags/bmb-tags.component';
   changeDetection: ChangeDetectionStrategy.OnPush,
   encapsulation: ViewEncapsulation.None,
 })
-export class BmbDropdownComponent implements AfterViewInit, ControlValueAccessor, OnChanges, OnInit {
-
+export class BmbDropdownComponent
+  implements AfterViewInit, ControlValueAccessor, OnChanges, OnInit
+{
   @Input() required?: boolean;
   @Input() showIcon?: boolean = false;
   @Input() placeholder?: string = '';
@@ -43,7 +67,7 @@ export class BmbDropdownComponent implements AfterViewInit, ControlValueAccessor
   @Input() formControl?: FormControl | undefined;
   @Input() disabled?: boolean = false;
   @Input() label?: string;
-  @Input() type?: 'default' | 'autocomplete' = 'default'
+  @Input() type?: 'default' | 'autocomplete' = 'default';
 
   @Output() onValueChange: EventEmitter<any> = new EventEmitter<any>();
 
@@ -51,7 +75,6 @@ export class BmbDropdownComponent implements AfterViewInit, ControlValueAccessor
   inputWrapper: ElementRef | undefined;
 
   @ViewChild('filterDropdown') filterField: ElementRef | null = null;
-
 
   @ViewChild('input', { static: false })
   input: NgModel | undefined;
@@ -62,7 +85,7 @@ export class BmbDropdownComponent implements AfterViewInit, ControlValueAccessor
   onClick(event: MouseEvent) {
     if (!this.childNodes?.contains(event.target)) {
       this.openSelect = false;
-      this.isFocus = this.isFocus == false ? false : true
+      this.isFocus = this.isFocus == false ? false : true;
     }
   }
 
@@ -73,26 +96,26 @@ export class BmbDropdownComponent implements AfterViewInit, ControlValueAccessor
   multipleOptions?: string[] = [];
 
   uid: string = Date.now().toString(36) + (Math.floor(Math.random() * 90) + 10);
-  filterControl= new FormControl();
+  filterControl = new FormControl();
   filteredData: string[] = [];
-  
-  value: string = '';
-  openSelect: boolean = false
 
-  constructor(private elementRef: ElementRef,){}
+  value: string = '';
+  openSelect: boolean = false;
+
+  constructor(private elementRef: ElementRef) {}
 
   ngOnInit(): void {
-    if(this.formControl === undefined){
-      this.formControl = new FormControl()
+    if (this.formControl === undefined) {
+      this.formControl = new FormControl();
     }
   }
-  
+
   ngAfterViewInit(): void {
     this.childNodes = this.elementRef.nativeElement;
     if (this.disabled && this.formControl) {
       this.formControl.disable();
-    } else if(this.disabled && !this.formControl){
-      this.input?.disabled
+    } else if (this.disabled && !this.formControl) {
+      this.input?.disabled;
     }
   }
 
@@ -107,82 +130,83 @@ export class BmbDropdownComponent implements AfterViewInit, ControlValueAccessor
   }
 
   handleItemClick(event: string, index: any): void {
-    if(this.type == 'autocomplete'){
-      let found = this.multipleOptions?.find((element) => element == event )
-      if (found === undefined){
-        this.multipleOptions?.push(event)
-        this.onValueChange.emit(this.multipleOptions)
-        if(this.formControl){
-          this.formControl.setValue(this.multipleOptions)
+    if (this.type == 'autocomplete') {
+      let found = this.multipleOptions?.find((element) => element == event);
+      if (found === undefined) {
+        this.multipleOptions?.push(event);
+        this.onValueChange.emit(this.multipleOptions);
+        if (this.formControl) {
+          this.formControl.setValue(this.multipleOptions);
         }
       }
-    }else{
+    } else {
       this.onValueChange.emit(event);
-      this.selectedIndexOption = index
-      this.selectedOption = event
-      if(this.formControl){
-        this.formControl.setValue(event)
+      this.selectedIndexOption = index;
+      this.selectedOption = event;
+      if (this.formControl) {
+        this.formControl.setValue(event);
       }
     }
 
-    this.isFocus = !this.isFocus
+    this.isFocus = !this.isFocus;
     this.openSelect = false;
-
   }
 
-  openDialog(){
-    this.openSelect = !this.openSelect
-    this.isFocus = !this.isFocus
+  openDialog() {
+    this.openSelect = !this.openSelect;
+    this.isFocus = !this.isFocus;
   }
 
-  onParentClick(){
-    if(this.openSelect){
-      this.isFocus = !this.isFocus
+  onParentClick() {
+    if (this.openSelect) {
+      this.isFocus = !this.isFocus;
     }
   }
 
-  onKeyDown(event: KeyboardEvent){
-    if (['Enter', ' ', 'ArrowDown', 'Down', 'ArrowUp', 'Up'].indexOf(event.key) > -1) {
+  onKeyDown(event: KeyboardEvent) {
+    if (
+      ['Enter', ' ', 'ArrowDown', 'Down', 'ArrowUp', 'Up'].indexOf(event.key) >
+      -1
+    ) {
       if (!this.openSelect) {
-        this.openDialog()
+        this.openDialog();
       }
-    
+
       if (!this.options!.length) {
         event.preventDefault();
         return;
       }
     }
-
   }
 
   deleteTag(index: number) {
-    this.multipleOptions?.splice(index, 1)
-    this.onValueChange.emit(this.multipleOptions)
-    if(this.formControl){
-      this.formControl.setValue(this.multipleOptions)
+    this.multipleOptions?.splice(index, 1);
+    this.onValueChange.emit(this.multipleOptions);
+    if (this.formControl) {
+      this.formControl.setValue(this.multipleOptions);
     }
   }
 
   public onChangeFn = (_: any) => {};
- 
+
   public onTouchedFn = () => {};
- 
+
   public registerOnChange(fn: any): void {
     this.onChangeFn = fn;
   }
- 
+
   public registerOnTouched(fn: any): void {
     this.onTouchedFn = fn;
   }
- 
+
   public writeValue(obj: any): void {
     this.value = obj;
   }
- 
+
   public onTouched() {
     this.onTouchedFn();
   }
- 
+
   public onChange() {
     this.onChangeFn(this.value);
   }
