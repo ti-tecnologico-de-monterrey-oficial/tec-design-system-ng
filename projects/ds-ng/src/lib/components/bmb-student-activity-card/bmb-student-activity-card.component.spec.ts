c76ed98eb833be038b414f436ba42a0be373d1bd
Castro Enrique <castroEnriqueJr22@gmail.com>
2024-07-01 10:19:35 -0600

    feature: component student activity card

---
+++ projects/ds-ng/src/lib/components/bmb-student-activity-card/bmb-student-activity-card.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { BmbStudentActivityCardComponent } from './bmb-student-activity-card.component';
+
+describe('BmbStudentActivityCardComponent', () => {
+  let component: BmbStudentActivityCardComponent;
+  let fixture: ComponentFixture<BmbStudentActivityCardComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [BmbStudentActivityCardComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(BmbStudentActivityCardComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
