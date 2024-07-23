1ef8379e252c5b67767137297b7434b7b6ecfd64
LizTec57 <t-lizvillegas@itesm.mx>
2024-07-23 11:13:49 -0600

    SideBar

--- src/app/emprendedor/emprendedor.component.ts
+++ src/app/emprendedor/emprendedor.component.ts
@@ -1,22 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { BmbSidebarComponent } from '../../../projects/ds-ng/src/public-api';
+import { SidebarElement } from '../../../projects/ds-ng/src/lib/components/bmb-sidebar/bmb-sidebar.interface';
 
 @Component({
   selector: 'bmb-emprendedor',
   standalone: true,
   imports: [RouterModule, BmbSidebarComponent],
   templateUrl: './emprendedor.component.html',
-  styleUrl: './emprendedor.component.scss',
+  styleUrls: ['./emprendedor.component.scss'],
 })
 export default class EmprendedorComponent {
-  sidebarElements: {
-    id: number;
-    icon: string;
-    title: string;
-    link: string;
-    target?: string;
-  }[][] = [
+  title = 'Navegacion para mobiles';
+  sidebarElements: SidebarElement[][] = [
     [
       {
         id: 1,
@@ -40,16 +36,74 @@ export default class EmprendedorComponent {
       {
         id: 4,
         icon: 'checklist_rtl',
-        title: 'Estado de archivos',
+        title: 'Estado de archivos anonimos',
         link: 'inicio',
+        children: [
+          {
+            id: 4.1,
+            icon: 'file_copy',
+            title: 'Sub-item 1',
+            link: '/emprendedor',
+          },
+          {
+            id: 4.2,
+            icon: 'file_copy',
+            title: 'Sub-item 2',
+            link: 'https://www.youtube.com/watch?v=beh56CrNRsQ',
+          },
+        ],
+      },
+      {
+        id: 5,
+        icon: 'account_box',
+        title: 'Admin.usuariosusuariosusuarios',
+        link: 'vivencia',
+        children: [
+          {
+            id: 5.1,
+            icon: 'file_copy',
+            title: 'Sub-item 3',
+            link: '/home',
+          },
+          {
+            id: 5.2,
+            icon: 'file_copy',
+            title: 'Sub-item 4',
+            link: 'https://www.youtube.com/watch?v=beh56CrNRsQ',
+          },
+        ],
       },
     ],
     [
       {
-        id: 5,
+        id: 7,
         icon: 'account_box',
         title: 'Admin. usuarios',
-        link: 'vivencia',
+        link: 'vivencia1',
+      },
+      {
+        id: 8,
+        icon: 'account_box',
+        title: 'Admin. usuarios',
+        link: 'vivencia2',
+      },
+      {
+        id: 9,
+        icon: 'account_box',
+        title: 'Admin. usuarios',
+        link: 'vivencia3',
+      },
+      {
+        id: 10,
+        icon: 'account_box',
+        title: 'Hi',
+        link: 'vivencia4',
+      },
+      {
+        id: 11,
+        icon: 'account_box',
+        title: 'Hi',
+        link: 'vivencia5',
       },
     ],
   ];
