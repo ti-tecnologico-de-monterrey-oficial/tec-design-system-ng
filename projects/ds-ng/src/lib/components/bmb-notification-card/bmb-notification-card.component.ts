b8a5a0b1bdcf41a2965c0405ef84ceea6598c0e2
t-havila <t-hazelavila@itesm.mx>
2024-08-01 00:46:41 -0600

    feat: actulizar componente y se agrega story

--- projects/ds-ng/src/lib/components/bmb-notification-card/bmb-notification-card.component.ts
+++ projects/ds-ng/src/lib/components/bmb-notification-card/bmb-notification-card.component.ts
@@ -1,6 +1,4 @@
 import { ChangeDetectionStrategy, Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
-import { BmbHomeCardHeaderComponent } from '../bmb-home-card/bmb-home-card-header/bmb-home-card-header.component';
-import { BmbHomeCardComponent } from '../bmb-home-card/bmb-home-card.component';
 import { BmbBadgeComponent, BmbIconComponent } from 'ds-ng';
 import { IBmbNotificationCardData } from './types';
 import { CommonModule } from '@angular/common';
@@ -16,67 +14,40 @@ import { CommonModule } from '@angular/common';
 })
 export class BmbNotificationCardComponent implements OnInit {
 
-  @Input() data: IBmbNotificationCardData = {
-    new: [
-      {
-        description: 'Descripcion Corta',
-        time: '4d 12h'
-      },
-    ],
-    all: [
-      {
-        description: 'Descripcion Corta',
-        time: '4d 12h'
-      },
-      {
-        description: 'Descripcion Corta',
-        time: '4d 12h'
-      },
-    ],
-    seen: [
-      {
-        description: 'Descripcion Corta',
-        time: '4d 12h'
-      },
-      {
-        description: 'Descripcion Corta',
-        time: '4d 12h'
-      },
-      {
-        description: 'Descripcion Corta',
-        time: '4d 12h'
-      },
-    ]
-  };
+  @Input() data: IBmbNotificationCardData | null = null
 
   expanded: boolean = false;
-  activeData: any = null;
+  activeData: any = [];
   activeTab: number = 1;
   activeDot: number = 0;
 
 
   ngOnInit(): void {
     this.activeTab = 1;
-    this.activeData = this.data.new;
+    if(this.data !== null){
+      this.activeData = this.data!.new;
+    }
   }
 
   setActiveTab(tab: number) {
     this.activeTab = tab;
 
-    switch (tab) {
-      case 1:
-        this.activeData = this.data.new;
-        break;
-      case 2:
-        this.activeData = this.data.all;
-        break;
-      case 3:
-        this.activeData = this.data.seen;
-        break;
-
-      default:
-        this.activeData = this.data.new;
-        break;
+    if(this.data !== null){
+      switch (tab) {
+        case 1:
+          this.activeData = this.data!.new;
+          break;
+        case 2:
+          this.activeData = this.data!.all;
+          break;
+        case 3:
+          this.activeData = this.data!.seen;
+          break;
+  
+        default:
+          this.activeData = this.data!.new;
+          break;
+      }
     }
   }
 
