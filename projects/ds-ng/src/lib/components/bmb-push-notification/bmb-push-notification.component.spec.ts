d43b64a3fc19c2bd3e027637c98230d8fa217a62
Juan Pedro Sanchez <165101597+t-juansanchez@users.noreply.github.com>
2024-08-09 14:01:21 -0600

    Feature: Added Push notification component

---
+++ projects/ds-ng/src/lib/components/bmb-push-notification/bmb-push-notification.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { BmbPushNotificationComponent } from './bmb-push-notification.component';
+
+describe('BmbPushNotificationComponent', () => {
+  let component: BmbPushNotificationComponent;
+  let fixture: ComponentFixture<BmbPushNotificationComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [BmbPushNotificationComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(BmbPushNotificationComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
