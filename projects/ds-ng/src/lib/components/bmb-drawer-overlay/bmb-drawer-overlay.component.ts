ca3666ae9bf7265eadf7f4b3d01a75bbb479c395
LizTec57 <t-lizvillegas@itesm.mx>
2024-08-21 11:35:14 -0600

    drawer overlay

---
+++ projects/ds-ng/src/lib/components/bmb-drawer-overlay/bmb-drawer-overlay.component.ts
@@ -0,0 +1,69 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Output,
+  Input,
+  EventEmitter,
+  ViewEncapsulation,
+} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BmbFabComponent } from '../bmb-fab/bmb-fab.component';
+import { BmbInteractiveIconComponent } from '../bmb-interactive-icon/bmb-interactive-icon.component';
+import { IBmbApp } from '../../types';
+import { BmbInnerHeaderComponent } from '../bmb-inner-header/bmb-inner-header.component';
+import { BmbTabsComponent } from '../bmb-tabs/bmb-tabs.component';
+
+@Component({
+  selector: 'bmb-drawer-overlay',
+  standalone: true,
+  imports: [
+    CommonModule,
+    BmbFabComponent,
+    BmbInteractiveIconComponent,
+    BmbInnerHeaderComponent,
+    BmbTabsComponent,
+  ],
+  styleUrls: ['./bmb-drawer-overlay.component.scss'],
+  templateUrl: './bmb-drawer-overlay.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  encapsulation: ViewEncapsulation.None,
+})
+export class BmbDrawerOverlayComponent {
+  @Input() menu: any = [];
+  @Input() title: string = '';
+  @Input() dataSearch: string[] = [];
+  @Input() tabs: { title: string; id: number }[] = [];
+  @Input() appServices: { [key: number]: IBmbApp[] } = {};
+
+  @Output() onValueChange: EventEmitter<string> = new EventEmitter<string>();
+
+  isOpen: boolean = false;
+  isFull: boolean = false;
+  activeNavItemIndex: number = 0;
+
+  toggleDrawer() {
+    this.isOpen = !this.isOpen;
+
+    if (!this.isOpen) {
+      this.isFull = false;
+    }
+  }
+
+  toggleFullDrawer() {
+    this.isFull = !this.isFull;
+    this.isOpen = !this.isFull;
+  }
+
+  selectNavItem(index: number) {
+    this.activeNavItemIndex = index;
+  }
+
+  handleValueChange(event: string): void {
+    this.onValueChange.emit(event);
+  }
+
+  get appsActive(): IBmbApp[] {
+    const appsForCurrentNav = this.appServices[this.activeNavItemIndex];
+    return Array.isArray(appsForCurrentNav) ? appsForCurrentNav : [];
+  }
+}
