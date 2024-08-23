eba9c2736d95f1068ac8342b3d59cb500ba0312a
Juan Pedro Sanchez <165101597+t-juansanchez@users.noreply.github.com>
2024-08-23 09:33:34 -0600

    Chore: partial changes

--- projects/ds-ng/src/lib/components/bmb-account-statement/bmb-account-statement.component.ts
+++ projects/ds-ng/src/lib/components/bmb-account-statement/bmb-account-statement.component.ts
@@ -7,11 +7,24 @@ import {
 } from '@angular/core';
 import { BmbHeaderMobileComponent } from '../bmb-header-mobile/bmb-header-mobile.component';
 import { BmbBalanceOverviewComponent } from '../bmb-balance-overview/bmb-balance-overview.component';
+import { BmbCardComponent, BmbCardContentComponent } from '../bmb-card/bmb-card.component';
+import { BmbProgressBarComponent } from '../bmb-progress-bar/bmb-progress-bar.component';
+import { DateTime } from 'luxon';
+import { BmbDividerComponent } from '../bmb-divider/bmb-divider.component';
+import { BmbButtonDirective } from '../../directives/button.directive';
 
 @Component({
   selector: 'bmb-account-statement',
   standalone: true,
-  imports: [BmbHeaderMobileComponent, BmbBalanceOverviewComponent],
+  imports: [
+    BmbHeaderMobileComponent,
+    BmbBalanceOverviewComponent,
+    BmbCardComponent,
+    BmbCardContentComponent,
+    BmbProgressBarComponent,
+    BmbDividerComponent,
+    BmbButtonDirective,
+  ],
   templateUrl: './bmb-account-statement.component.html',
   styleUrl: './bmb-account-statement.component.scss',
   encapsulation: ViewEncapsulation.None,
@@ -22,12 +35,30 @@ export class BmbAccountStatementComponent {
   progressCircleTitle = input<string[]>(['Total a pagar', 'este mes']);
   labelPrimary = input<string>('Cuota Mensual');
   labelSecondary = input<string>('Pendiente');
-  valuePrimary = input<number>(0);
-  valueSecondary = input<number>(0);
+  totalCount = input<number>(0);
+  counter = input<number>(0);
+  progressTitle = input<string>('Total pagado');
+  formatDates = input<string>('yyyy-MM-dd');
+  paymentDeadline = input<string>();
+  cutOffDate = input<string>();
+  paymentDeadlineLabel = input<string>('Fecha límite de pago:');
+  cutOffDateLabel = input<string>('Fecha de corte:');
 
   closeEvent = output<void>();
+  backEvent = output<void>();
+
 
   handleClose() {
     this.closeEvent.emit();
+  }
+
+  handleBack() {
+    this.backEvent.emit();
+  }
+
+  getFormatedDate(date?: string): string {
+    if (!date) return '';
+    const formatDate = DateTime.fromFormat(date, this.formatDates());
+    return formatDate.toLocaleString({ day: '2-digit', month: 'short', year: 'numeric' });
   }
 }
