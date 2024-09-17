098cd825c42ec229cfe81262cb46a172e8615b25
t-havila <t-hazelavila@itesm.mx>
2024-09-17 15:51:54 -0600

    doc: Se actualiza documentacion para propiedades obsoletas

--- projects/ds-ng/src/lib/components/bmb-tags/bmb-tags.stories.ts
+++ projects/ds-ng/src/lib/components/bmb-tags/bmb-tags.stories.ts
@@ -52,6 +52,9 @@ Below is an example of how you can use this component in HTML:
         category: 'Properties',
         type: { summary: 'boolean' },
         defaultValue: { summary: false },
+        deprecated: {
+          summary: 'This property is deprecated and will be removed in future versions..',
+        },
       },
     },
     activityTag: {
