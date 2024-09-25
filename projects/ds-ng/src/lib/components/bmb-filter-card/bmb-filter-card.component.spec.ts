94a25efbf1b7890aecc10c489e85ce1bd91f6c54
LizTec57 <t-lizvillegas@itesm.mx>
2024-09-24 19:00:11 -0600

    changes

--- projects/ds-ng/src/lib/components/bmb-filter-card/bmb-filter-card.component.spec.ts
+++ projects/ds-ng/src/lib/components/bmb-filter-card/bmb-filter-card.component.spec.ts
@@ -1,42 +1,16 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
 import { BmbFilterCardComponent } from './bmb-filter-card.component';
-import { FormsModule } from '@angular/forms';
-import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 describe('BmbFilterCardComponent', () => {
   let component: BmbFilterCardComponent;
   let fixture: ComponentFixture<BmbFilterCardComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [BmbFilterCardComponent],
-      imports: [FormsModule],
-      schemas: [NO_ERRORS_SCHEMA],
-    }).compileComponents();
-
+  beforeEach(() => {
     fixture = TestBed.createComponent(BmbFilterCardComponent);
     component = fixture.componentInstance;
-    component.tags = ['Competencia 1', 'Competencia 2', 'Competencia 3'];
-    fixture.detectChanges();
   });
 
-  it('should create the component', () => {
+  it('should create', () => {
     expect(component).toBeTruthy();
-  });
-
-  it('should filter tags based on search query', () => {
-    const searchQuery = 'Competencia 1';
-    component.onSearch(searchQuery);
-    expect(component.filteredTags).toEqual(['Competencia 1']);
-  });
-
-  it('should toggle the state of a filter option', () => {
-    const indexToTest = 0;
-    const initialCheckedState = component.options[indexToTest].checked;
-
-    component.handleToggleOption(indexToTest);
-    fixture.detectChanges();
-
-    expect(component.options[indexToTest].checked).toBe(!initialCheckedState);
   });
 });
