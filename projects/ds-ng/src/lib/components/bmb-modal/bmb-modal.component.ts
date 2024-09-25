ef3acb7a236048aadd2b503ebccebadbeb053e0f
Lupita Ascencio <t-marascencio@itesm.mx>
2024-09-25 11:14:10 -0600

    feat: Homologar modal, some optimizations

--- projects/ds-ng/src/lib/components/bmb-modal/bmb-modal.component.ts
+++ projects/ds-ng/src/lib/components/bmb-modal/bmb-modal.component.ts
@@ -12,22 +12,20 @@ import {
   MatDialog,
 } from '@angular/material/dialog';
 import { ModalDataConfig } from './bmb-modal.interface';
-import { BmbIconComponent } from '../bmb-icon/bmb-icon.component';
 import { BmbButtonDirective } from '../../directives/button.directive';
 import { ModalService } from '../../services/modal.service';
 import {
   BmbHeaderSectionComponent,
   IBmbHeaderAction,
 } from '../bmb-header-section/bmb-header-section.component';
-
-export type IBmbModalSize = 'small' | 'medium' | 'large';
+import { BmbContainerComponent } from '../bmb-container/bmb-container.component';
 
 @Component({
   selector: 'bmb-modal',
   standalone: true,
   imports: [
     CommonModule,
-    BmbIconComponent,
+    BmbContainerComponent,
     BmbButtonDirective,
     BmbHeaderSectionComponent,
   ],
@@ -39,13 +37,11 @@ export type IBmbModalSize = 'small' | 'medium' | 'large';
 })
 export class BmbModalComponent {
   svgUrl: string = '../../../assets/svg/';
-  btnColor: any = 'bmb_modal-btn-main';
-  isTemplate: boolean = false;
   modalTemplate: TemplateRef<any> | null = null;
   headerActions: IBmbHeaderAction[] = [
     {
       icon: 'close',
-      action: () => {},
+      action: () => this.closeModal(true),
     },
   ];
 
@@ -55,48 +51,127 @@ export class BmbModalComponent {
   ) {}
 
   ngOnInit() {
-    this.setConfig(this.modalData);
-  }
-
-  actionFunction() {
-    this.closeModal();
-  }
-
-  closeModal() {
-    this.dialogRef.close();
-  }
-
-  getButtonClass(alertStyle: string): string {
-    if (!!alertStyle) {
-      return `bmb_modal-btn-${alertStyle}`;
-    }
-
-    return 'bmb_modal-btn-primary';
-  }
-
-  setConfig(data: ModalDataConfig) {
-    if (data.type == 'alert') {
-      this.btnColor = this.getButtonClass(data.alertStyle!);
-      this.setImage(data.alertStyle!);
-    }
-
+    const data: ModalDataConfig = this.getData();
     if (typeof data.content !== 'string') {
       this.modalTemplate = data.content!;
-      this.isTemplate = true;
     }
   }
 
-  setImage(alertStyle: string) {
-    if ('primary') return `${this.svgUrl}info_fill_${alertStyle}.svg`;
-    if ('neutral') return `${this.svgUrl}info_fill.svg`;
-    if (
-      alertStyle === 'error' ||
-      alertStyle === 'event' ||
-      alertStyle === 'success' ||
-      alertStyle === 'warning'
-    )
-      return `${this.svgUrl}${alertStyle}_fill.svg`;
+  closeModal(event: boolean) {
+    this.dialogRef.close(event);
+  }
 
-    return `${this.svgUrl}info_fill.svg`;
+  isModalTemplate(): boolean {
+    return !!this.modalTemplate;
+  }
+
+  getModalClasses(): string[] {
+    const baseClassName: string = 'bmb_modal';
+    const classNames: string[] = [baseClassName];
+
+    if (!!this.getData().size) {
+      classNames.push(`${baseClassName}-size-${this.getData().size}`);
+    }
+
+    return classNames;
+  }
+
+  getDescriptionClasses(): string[] {
+    const baseClassName: string = 'bmb_modal-content';
+    const classNames: string[] = [`${baseClassName}-description`];
+
+    if (!!this.getData().scrollable) {
+      classNames.push(`${baseClassName}-scrollable`);
+    }
+
+    return classNames;
+  }
+
+  getButtonClass(isSecondaryButton: boolean): string[] {
+    const data: ModalDataConfig = this.getData();
+    const footerClassName: string = 'bmb_modal-footer';
+    const baseClassName: string = `${footerClassName}-button`;
+    const classNames: string[] = [baseClassName];
+
+    if (isSecondaryButton) {
+      classNames.push(`${footerClassName}-btn`);
+      classNames.push(`${baseClassName}-secondary_action`);
+      return classNames;
+    }
+
+    if (data.type === 'alert') {
+      if (!!data.alertStyle) {
+        classNames.push(`${baseClassName}-${data.alertStyle}`);
+      } else {
+        classNames.push(`${baseClassName}-neutral`);
+      }
+    }
+
+    return classNames;
+  }
+
+  getData(): ModalDataConfig {
+    return this.modalData;
+  }
+
+  getImage(): string {
+    const data: ModalDataConfig = this.getData();
+    const alertStyle: string = data.alertStyle!;
+
+    if (data.type === 'alert') {
+      if (alertStyle === 'primary') {
+        return `${this.svgUrl}info_fill_${alertStyle}.svg`;
+      }
+      if (alertStyle === 'neutral') return `${this.svgUrl}info_fill.svg`;
+      if (
+        alertStyle === 'error' ||
+        alertStyle === 'event' ||
+        alertStyle === 'success' ||
+        alertStyle === 'warning'
+      )
+        return `${this.svgUrl}${alertStyle}_fill.svg`;
+
+      return `${this.svgUrl}info_fill.svg`;
+    }
+
+    return '';
+  }
+
+  getTitle(): string {
+    return this.getData().title!;
+  }
+
+  getSubtitle(): string {
+    return this.getData().subtitle!;
+  }
+
+  getHeaderActions(): IBmbHeaderAction[] {
+    return this.headerActions;
+  }
+
+  getContent(): any {
+    if (this.isModalTemplate()) return this.modalTemplate;
+    return this.getData().content;
+  }
+
+  getPrimaryBtnLabel(): string {
+    return this.getData().primaryBtnLabel || 'OK';
+  }
+
+  getSecondaryBtnLabel(): string {
+    return this.getData().secondaryBtnLabel!;
+  }
+
+  showFooter(): boolean {
+    return this.getData().type !== 'informative';
+  }
+
+  showPrimaryButton(): boolean {
+    return !this.getData().hidePrimaryButton;
+  }
+
+  showSecondaryButton(): boolean {
+    const data = this.getData();
+    return data.type !== 'action' || !!data.secondaryBtnLabel;
   }
 }
--- projects/ds-ng/src/lib/components/bmb-modal/bmb-modal.interface.ts
+++ projects/ds-ng/src/lib/components/bmb-modal/bmb-modal.interface.ts
@@ -1,18 +1,21 @@
 import { TemplateRef } from '@angular/core';
 
+export type IBmbModalSize = 'small' | 'medium' | 'large';
+export type IBmbModalType = 'informative' | 'action' | 'alert';
+export type IBmbModalAlertStyle =
+  | 'warning'
+  | 'neutral'
+  | 'primary'
+  | 'event'
+  | 'success'
+  | 'error';
 export interface ModalDataConfig {
   title?: string;
   subtitle?: string;
   content?: string | TemplateRef<any> | null;
-  type?: 'informative' | 'action' | 'alert';
-  alertStyle?:
-    | 'warning'
-    | 'neutral'
-    | 'primary'
-    | 'event'
-    | 'success'
-    | 'error';
-  size?: 'small' | 'medium' | 'large';
+  type?: IBmbModalType;
+  alertStyle?: IBmbModalAlertStyle;
+  size?: IBmbModalSize;
   primaryBtnLabel?: string;
   secondaryBtnLabel?: string;
   scrollable?: boolean;
