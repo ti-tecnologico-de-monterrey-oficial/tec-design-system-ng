bf9df6a07b82ed121d6e131bceeef817066fa693
Lupita Ascencio <t-marascencio@itesm.mx>
2024-09-27 19:43:06 -0600

    feat: Modal componente, optimization code

--- projects/ds-ng/src/lib/components/bmb-modal/bmb-modal.component.ts
+++ projects/ds-ng/src/lib/components/bmb-modal/bmb-modal.component.ts
@@ -62,12 +62,10 @@ export class BmbModalComponent {
 
     if (buttonName === 'primary' && data.primaryAction) {
       data.primaryAction();
-      return;
     }
 
     if (buttonName === 'secondary' && data.secondaryAction) {
       data.secondaryAction();
-      return;
     }
 
     this.dialogRef.close(event);
