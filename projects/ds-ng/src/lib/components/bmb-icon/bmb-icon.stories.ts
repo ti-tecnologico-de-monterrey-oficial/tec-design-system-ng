e0391bd7c5dcdafa3415a588e56d380ff4424980
Lupita Ascencio <t-marascencio@itesm.mx>
2024-10-15 13:21:37 -0600

    merge into feature prod

--- projects/ds-ng/src/lib/components/bmb-icon/bmb-icon.stories.ts
+++ projects/ds-ng/src/lib/components/bmb-icon/bmb-icon.stories.ts
@@ -49,7 +49,8 @@ Below is an example of how you can use this component in HTML:
     dotNotification: {
       name: 'Dot Notification',
       control: { type: 'number' },
-      description: 'Set a dot with the number of notifications in the bottom right of the icon.',
+      description:
+        'Set a dot with the number of notifications in the bottom right of the icon.',
       table: {
         category: 'Properties',
         type: { summary: 'number' },
