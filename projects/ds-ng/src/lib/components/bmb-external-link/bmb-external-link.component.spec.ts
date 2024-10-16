ef0c756cfa91bdbc70bab4c770a61b46a77148b1
Lupita Ascencio <t-marascencio@itesm.mx>
2024-10-16 13:59:50 -0600

    feat: some fixes

--- projects/ds-ng/src/lib/components/bmb-external-link/bmb-external-link.component.spec.ts
+++ projects/ds-ng/src/lib/components/bmb-external-link/bmb-external-link.component.spec.ts
@@ -1,6 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { BmbExternalLinkComponent } from './bmb-external-link.component';
+import {
+  BmbExternalLinkComponent,
+  IBmbMenuEvent,
+} from './bmb-external-link.component';
 import { ComponentRef } from '@angular/core';
+import { IBmbFooterEvent } from '../bmb-bottom-navigation-bar/bmb-bottom-navigation-bar.component';
 
 describe('BmbExternalLinkComponent', () => {
   let component: BmbExternalLinkComponent;
@@ -15,12 +19,72 @@ describe('BmbExternalLinkComponent', () => {
     fixture = TestBed.createComponent(BmbExternalLinkComponent);
     component = fixture.componentInstance;
     componentRef = fixture.componentRef;
-    componentRef.setInput('title', 'Title');
-    componentRef.setInput('subtitle', 'Subtitle');
+    componentRef.setInput('title', 'Test Title');
+    componentRef.setInput('subtitle', 'https://www.example.com');
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create the component', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display the provided title', () => {
+    componentRef.setInput('title', 'Test Title');
+    fixture.detectChanges();
+    const titleElement = fixture.nativeElement.querySelector(
+      'span.bmb_chevron-content-title',
+    );
+    expect(titleElement.textContent).toBe('Test Title');
+  });
+
+  it('should display the provided subtitle', () => {
+    componentRef.setInput('subtitle', 'Test subtitle');
+    fixture.detectChanges();
+    const subtitleElement = fixture.nativeElement.querySelector(
+      'span.bmb_chevron-content-subtitle',
+    );
+    expect(subtitleElement.textContent.trim()).toBe('Test subtitle');
+  });
+
+  it('should emit onClose event when handleClose is called', () => {
+    spyOn(component.onClose, 'emit');
+    const event = { some: 'data' };
+    component.handleClose(event);
+    expect(component.onClose.emit).toHaveBeenCalledWith(event);
+  });
+
+  it('should toggle showMenu when handleOpenMenu is called', () => {
+    expect(component.showMenu).toBeFalse();
+    component.handleOpenMenu();
+    expect(component.showMenu).toBeTrue();
+    component.handleOpenMenu();
+    expect(component.showMenu).toBeFalse();
+  });
+
+  it('should emit menuEvent on menu option click', () => {
+    spyOn(component.menuEvent, 'emit');
+    const event: IBmbMenuEvent = 'link';
+    component.onMenuOptionClick(event);
+    expect(component.menuEvent.emit).toHaveBeenCalledWith(event);
+    expect(component.showMenu).toBeFalse();
+  });
+
+  it('should emit footerEvent on footer option click', () => {
+    spyOn(component.footerEvent, 'emit');
+    const event: IBmbFooterEvent = 'back';
+    component.onFooterOptionClick(event);
+    expect(component.footerEvent.emit).toHaveBeenCalledWith(event);
+  });
+
+  it('should return true for isIconSubtitle if subtitle is a URL', () => {
+    componentRef.setInput('subtitle', 'https://www.example.com');
+    fixture.detectChanges();
+    expect(component.isIconSubtitle()).toBeTrue();
+  });
+
+  it('should return false for isIconSubtitle if subtitle is not a URL', () => {
+    componentRef.setInput('subtitle', 'Not a URL');
+    fixture.detectChanges();
+    expect(component.isIconSubtitle()).toBeFalse();
   });
 });
--- projects/ds-ng/src/lib/components/bmb-external-link/bmb-external-link.component.stories.ts
+++ projects/ds-ng/src/lib/components/bmb-external-link/bmb-external-link.component.stories.ts
@@ -36,7 +36,7 @@ Below is an example of how you can use this component in HTML:
       description: 'Header title',
       table: {
         category: 'Properties',
-        type: { summary: 'string' },
+        type: { summary: 'string (required)' },
       },
     },
     subtitle: {
@@ -47,13 +47,48 @@ Below is an example of how you can use this component in HTML:
       description: 'Header subtitle/url',
       table: {
         category: 'Properties',
-        type: { summary: 'string' },
+        type: { summary: 'string (required)' },
+      },
+    },
+    navigationBarIcons: {
+      name: 'Configuration for the navigation bar icons.',
+      control: {
+        type: 'object',
+      },
+      description: `
+        Header subtitle. The subtitle can also be a URL.
+        IBmbNavigationBarIcons = {
+          one: IBmbNavigationBarIcon;
+          two: IBmbNavigationBarIcon;
+          three: IBmbNavigationBarIcon;
+          four: IBmbNavigationBarIcon;
+        }
+        IBmbNavigationBarIcon = {
+        name: string;
+        label: string;
+        dotNotification?: number;
+        }
+      `,
+      table: {
+        category: 'Properties',
+        type: { summary: 'IBmbNavigationBarIcons (optional)' },
+        defaultValue: {
+          summary: `
+          {
+            one: { name: 'arrow_back_ios', label: '' },
+            two: { name: 'arrow_forward_ios', label: '' },
+            three: { name: 'share', label: '' },
+            four: { name: 'refresh', label: '' },
+          }
+        `,
+        },
       },
     },
     onClose: {
       name: 'Close event',
       control: false,
-      description: 'Close button event.',
+      description:
+        'Close button event. Emitted when the user closes the component.',
       table: {
         type: { summary: 'function' },
         category: 'Events',
@@ -63,7 +98,7 @@ Below is an example of how you can use this component in HTML:
       name: 'Menu events',
       control: false,
       description:
-        'Used for the menu option that was clicked. Options: link, openNew, info',
+        'Used for the menu option that was clicked. Options: link, openNew, info. Emitted when the user clicks on a menu options.',
       table: {
         type: { summary: 'function' },
         category: 'Events',
@@ -73,7 +108,7 @@ Below is an example of how you can use this component in HTML:
       name: 'Footer events',
       control: false,
       description:
-        'Used for the icon option that was clicked. Options: back, forward, share, reload',
+        'Used for the icon option that was clicked. Options: back, forward, share, reload. Emitted when the user clicks on a footer options.',
       table: {
         type: { summary: 'function' },
         category: 'Events',
@@ -83,6 +118,12 @@ Below is an example of how you can use this component in HTML:
   args: {
     title: 'CONECTA',
     subtitle: 'https://www.CONECTA.tec.mx',
+    navigationBarIcons: {
+      one: { name: 'arrow_back_ios', label: '' },
+      two: { name: 'arrow_forward_ios', label: '' },
+      three: { name: 'share', label: '' },
+      four: { name: 'refresh', label: '' },
+    },
     onClose: (event: unknown) => {
       alert('Selection: close');
     },
