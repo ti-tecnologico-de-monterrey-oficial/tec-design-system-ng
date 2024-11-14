a70146ba1a7d19c8f62255f0dfe353c4e321207e
LizTec57 <t-lizvillegas@itesm.mx>
2024-11-14 08:23:10 -0600

    container button

--- projects/ds-ng/src/lib/components/bmb-container-button/bmb-container-button.stories.ts
+++ projects/ds-ng/src/lib/components/bmb-container-button/bmb-container-button.stories.ts
@@ -2,6 +2,25 @@ import { Meta, StoryObj, moduleMetadata } from '@storybook/angular';
 import { BmbIconComponent } from '../bmb-icon/bmb-icon.component';
 import { BmbContainerButtonComponent } from './bmb-container-button.component';
 import { BmbContainerComponent } from '../bmb-container/bmb-container.component';
+import { IBbmBgAppearance } from '../bmb-advertisement-card/types';
+
+const appearanceOptions: IBbmBgAppearance[] = [
+  'normal',
+  'strong',
+  'success',
+  'info',
+  'warning',
+  'error',
+  'brand',
+  'background',
+  'disabled',
+  'mitec_blue',
+  'mitec_red',
+  'mitec_green',
+  'mitec_orange',
+  'mitec_light_green',
+  'mitec_purple',
+];
 
 export default {
   title: 'Macro Componentes/Container Button',
@@ -91,6 +110,7 @@ Below is an example of how you can use this component in HTML:
         'Name of the icon to use. Please use Material icons: https://fonts.google.com/icons. Do not use the score property if you want to the iconLeft.',
       table: {
         category: 'Properties',
+        defaultValue: { summary: 'optional' },
         type: { summary: 'string' },
       },
     },
@@ -101,6 +121,7 @@ Below is an example of how you can use this component in HTML:
         'Name of the icon to use. Please use Material icons: https://fonts.google.com/icons.',
       table: {
         category: 'Properties',
+        defaultValue: { summary: 'optional' },
         type: { summary: 'string' },
       },
     },
@@ -113,6 +134,7 @@ Below is an example of how you can use this component in HTML:
         'The score number of the button container. Do not use the iconLeft property if you want to use score.',
       table: {
         category: 'Properties',
+        defaultValue: { summary: 'optional' },
         type: { summary: 'string' },
       },
     },
@@ -127,16 +149,83 @@ Below is an example of how you can use this component in HTML:
         type: { summary: 'boolean' },
       },
     },
+    small: {
+      name: 'Small',
+      control: { type: 'boolean' },
+      description:
+        'When set to true, the container button will be small (160x80 pixels) and only show the icon/grade and title.',
+      table: {
+        category: 'Properties',
+        defaultValue: { summary: 'false' },
+        type: { summary: 'boolean' },
+      },
+    },
+    state: {
+      name: 'State',
+      control: { type: 'select' },
+      table: {
+        category: 'Properties',
+      },
+      options: ['disabled', 'error'],
+      description: 'The state of the button: disabled or error.',
+    },
+    alternative: {
+      name: 'Alternative',
+      control: { type: 'boolean' },
+      table: {
+        category: 'Properties',
+      },
+      description: 'If true, applies the alternative styles to the button.',
+    },
+    badgeText: {
+      name: 'Badge Text',
+      control: {
+        type: 'text',
+      },
+      description:
+        'The text of the badge. The width will increase depending on the length of the text.',
+      table: {
+        category: 'Properties',
+        defaultValue: { summary: 'optional' },
+        type: { summary: 'string' },
+      },
+    },
+    badgeAppearance: {
+      name: 'Badge Appearance',
+      control: {
+        type: 'select',
+      },
+      options: appearanceOptions,
+      table: {
+        category: 'Properties',
+        defaultValue: { summary: 'normal' },
+        type: { summary: 'string' },
+      },
+      description: 'The appearance of the badge, affecting its visual style.',
+    },
+    setButtonTemplate: {
+      name: 'Set Button Template',
+      control: { type: 'boolean' },
+      description: 'If true, sets a custom button template.',
+      table: {
+        category: 'Properties',
+        defaultValue: { summary: false },
+        type: { summary: 'boolean' },
+      },
+    },
+    onButton: {
+      action: 'buttonClick',
+      description: 'Event emitted when the button is clicked.',
+      table: {
+        category: 'Events',
+        type: { summary: 'EventEmitter<any>' },
+      },
+    },
   },
   args: {
     title: 'Tema de App',
-    subtitle: 'Subtitle',
     iconLeft: 'home',
     iconRight: 'chevron_right',
-    target: '_blank',
-    link: 'https://www.youtube.com/',
-    score: '',
-    square: false,
   },
 } as Meta<typeof BmbContainerButtonComponent>;
 
