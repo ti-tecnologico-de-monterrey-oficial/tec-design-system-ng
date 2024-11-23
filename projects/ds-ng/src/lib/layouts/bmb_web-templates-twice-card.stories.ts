22cd0e804813fe8dc51cc579f3106f14b3641b88
Juan Pedro Sanchez Miranda <t-juansanchez@itesm.mx>
2024-11-23 00:10:48 -0600

    Added new test

--- projects/ds-ng/src/lib/layouts/bmb_web-templates-twice-card.stories.ts
+++ projects/ds-ng/src/lib/layouts/bmb_web-templates-twice-card.stories.ts
@@ -7,25 +7,42 @@ import { BmbTopBarComponent } from '../../public-api';
   imports: [BmbTopBarComponent],
   selector: 'storybook-modal-wrapper',
   template: `
-    <bmb-top-bar
-    ></bmb-top-bar>
+    <bmb-top-bar></bmb-top-bar>
     <section class="bmb_template-header">
-        <h3>Twice Card</h3>
-        <h5>Template</h5>
+      <h3>Twice Card</h3>
+      <h5>Template</h5>
     </section>
     <div class="bmb_template-twice">
-        <main class="bmb_template-twice-main">
-            <h5>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi velit libero eveniet alias esse at perspiciatis minus quia aperiam enim? Commodi cum minima recusandae quidem blanditiis maiores, eaque perspiciatis at molestiae, dicta velit consequatur hic dolor! Nihil ipsa ullam, dolorum ut quod delectus fuga quam neque, velit tenetur corporis autem minima illo eum voluptas blanditiis esse nam obcaecati magni? Consequuntur rerum quas veritatis nobis maiores cumque ut hic consectetur iste quisquam, corporis molestias exercitationem dolore magni molestiae unde animi autem eos odit qui illum? Minus recusandae neque quia debitis nostrum aut, modi in omnis ad accusantium dolores vel eligendi labore.</h5>
-        </main>
-        <aside class="bmb_template-twice-secondary">
-            <h3 class="bmb_template-twice-title">Subheader</h3>
-            <h5>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aut ipsa repudiandae hic. Non, fugiat? Repellendus impedit soluta excepturi fugit doloribus. Quos, vel suscipit. Deleniti similique tempore at culpa facilis sunt laudantium, magni maxime dolor reprehenderit saepe! Quam eius, iste voluptate corporis sequi impedit fuga repudiandae amet placeat, delectus, quis sapiente?</h5>
-        </aside>
+      <main class="bmb_template-twice-main">
+        <h5>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi velit
+          libero eveniet alias esse at perspiciatis minus quia aperiam enim?
+          Commodi cum minima recusandae quidem blanditiis maiores, eaque
+          perspiciatis at molestiae, dicta velit consequatur hic dolor! Nihil
+          ipsa ullam, dolorum ut quod delectus fuga quam neque, velit tenetur
+          corporis autem minima illo eum voluptas blanditiis esse nam obcaecati
+          magni? Consequuntur rerum quas veritatis nobis maiores cumque ut hic
+          consectetur iste quisquam, corporis molestias exercitationem dolore
+          magni molestiae unde animi autem eos odit qui illum? Minus recusandae
+          neque quia debitis nostrum aut, modi in omnis ad accusantium dolores
+          vel eligendi labore.
+        </h5>
+      </main>
+      <aside class="bmb_template-twice-secondary">
+        <h3 class="bmb_template-twice-title">Subheader</h3>
+        <h5>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aut ipsa
+          repudiandae hic. Non, fugiat? Repellendus impedit soluta excepturi
+          fugit doloribus. Quos, vel suscipit. Deleniti similique tempore at
+          culpa facilis sunt laudantium, magni maxime dolor reprehenderit saepe!
+          Quam eius, iste voluptate corporis sequi impedit fuga repudiandae amet
+          placeat, delectus, quis sapiente?
+        </h5>
+      </aside>
     </div>
   `,
 })
-class StorybookModalWrapperComponent {
-}
+class StorybookModalWrapperComponent {}
 
 export default {
   title: 'Hidden/Web Templates/Twice Card',
@@ -33,10 +50,7 @@ export default {
   component: BmbTopBarComponent,
   decorators: [
     moduleMetadata({
-      imports: [
-        StorybookModalWrapperComponent,
-        BmbTopBarComponent
-      ],
+      imports: [StorybookModalWrapperComponent, BmbTopBarComponent],
       providers: [],
     }),
   ],
