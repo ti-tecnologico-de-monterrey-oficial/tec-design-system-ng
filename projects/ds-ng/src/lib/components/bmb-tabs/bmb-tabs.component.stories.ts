ca1f08dd9f858a35a7bcf82a9019211ec0364669
Juan Pedro Sanchez Miranda <t-juansanchez@itesm.mx>
2024-12-04 16:45:47 -0600

    Added: Portal component

--- projects/ds-ng/src/lib/components/bmb-tabs/bmb-tabs.component.stories.ts
+++ projects/ds-ng/src/lib/components/bmb-tabs/bmb-tabs.component.stories.ts
@@ -1,4 +1,4 @@
-import { Meta, StoryObj, moduleMetadata } from '@storybook/angular';
+import { Meta, StoryObj } from '@storybook/angular';
 import { BmbTabsComponent } from './bmb-tabs.component';
 
 export default {
