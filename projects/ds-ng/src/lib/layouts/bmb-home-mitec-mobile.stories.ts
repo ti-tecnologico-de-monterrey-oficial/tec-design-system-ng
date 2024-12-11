ef50c7eb90d850bf6b2562fef2a3b861743772ba
María Guadalupe Ascencio Escamilla <t-marascencio@itesm.mx>
2024-12-11 11:50:41 -0600

    Text was removed because the macro component should not appear until it is finished

--- projects/ds-ng/src/lib/layouts/bmb-home-mitec-mobile.stories.ts
+++ projects/ds-ng/src/lib/layouts/bmb-home-mitec-mobile.stories.ts
@@ -242,7 +242,7 @@ class StorybookHomeMitecMobile {
 }
 
 export default {
-  title: 'Macro Componentes/Home mitec/Mobile',
+  title: '',
   component: BmbUserProfileComponent,
   decorators: [
     moduleMetadata({
