095d47d9725d3917e2127772422331cd908d8b49
Juan Pedro Sanchez Miranda <t-juansanchez@itesm.mx>
2024-12-17 16:49:35 -0600

    Fix: Small fixes

--- projects/ds-ng/src/lib/components/bmb-login/bmb-login.component.ts
+++ projects/ds-ng/src/lib/components/bmb-login/bmb-login.component.ts
@@ -9,7 +9,7 @@ import { BmbButtonDirective } from '../../directives/button.directive';
 import { BmbInputComponent } from '../bmb-input/bmb-input.component';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { BmbUserProfileService } from '../../services/user/profile.service';
-import { BmbLoaderComponent } from 'ds-ng';
+import { BmbLoaderComponent } from '../bmb-loader/bmb-loader.component';
 import { BmbHeaderMitecComponent } from '../bmb-header-mitec/bmb-header-mitec.component';
 
 @Component({
