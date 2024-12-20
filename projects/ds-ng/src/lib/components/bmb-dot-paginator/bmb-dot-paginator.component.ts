ab39c77f883a492e534d87148584a6742d6b09db
María Guadalupe Ascencio Escamilla <t-marascencio@itesm.mx>
2024-12-20 12:18:47 -0600

    feat: Home mitec completed

--- projects/ds-ng/src/lib/components/bmb-dot-paginator/bmb-dot-paginator.component.ts
+++ projects/ds-ng/src/lib/components/bmb-dot-paginator/bmb-dot-paginator.component.ts
@@ -5,6 +5,9 @@ import {
   EventEmitter,
   ChangeDetectionStrategy,
   ViewEncapsulation,
+  input,
+  output,
+  model,
 } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
@@ -23,28 +26,28 @@ export interface Target {
   encapsulation: ViewEncapsulation.None,
 })
 export class BmbDotPaginatorComponent {
-  @Input() activeDotIndex?: number = 0;
-  @Input() totalDots?: number = 0;
-  @Input() targets: Target[] = [];
-  @Input() appearance: string = '';
-  @Output() onDotPress: EventEmitter<number> = new EventEmitter<number>();
+  activeDotIndex = model<number>(0);
+  totalDots = input<number>(0);
+  targets = input<Target[]>([]);
+  appearance = input<string>('');
+  onDotPress = output<number>();
 
   getDotsArray(): number[] {
-    return new Array(this.totalDots ?? 0).fill(0).map((_, i) => i);
+    return new Array(this.totalDots() ?? 0).fill(0).map((_, i) => i);
   }
 
   getClasses(): string[] {
     const classes: string[] = ['bmb_dot_paginator'];
 
-    if (this.appearance) {
-      classes.push('bmb_dot_paginator-' + this.appearance);
+    if (this.appearance()) {
+      classes.push('bmb_dot_paginator-' + this.appearance());
     }
 
     return classes;
   }
 
   onDotClicked(index: number): void {
-    this.activeDotIndex = index;
+    this.activeDotIndex.set(index);
     this.onDotPress.emit(index);
   }
 }
---
+++ projects/ds-ng/src/lib/components/bmb-login/bmb-login.stories.ts
@@ -0,0 +1,90 @@
+import { Meta, StoryObj } from '@storybook/angular';
+import { BmbLoginComponent } from './bmb-login.component';
+
+export default {
+  title: 'Macro Componentes/Login',
+  component: BmbLoginComponent,
+  decorators: [],
+  parameters: {
+    docs: {
+      description: {
+        component: `
+Below is an example of how you can use this component in TypeScript:
+
+\`\`\`typescript
+import { BmbLoginComponent } from '@ti-tecnologico-de-monterrey-oficial/ds-ng';
+@Component({
+  selector: 'component',
+  standalone: true,
+  imports: [ BmbLoginComponent ],
+  templateUrl: './component.html',
+  styleUrl: './component.scss',
+})
+\`\`\`
+
+Below is an example of how you can use this component in HTML:
+        `,
+      },
+    },
+  },
+  argTypes: {
+    headerLabel: {
+      name: 'Header label',
+      control: 'text',
+      description: 'Sets the label to be displayed in the header.',
+      table: {
+        category: 'Properties',
+        type: { summary: 'string' },
+      },
+    },
+    forgottenPasswordLabel: {
+      name: 'Forgotten password label',
+      control: 'text',
+      description: 'Sets the label to be displayed for forgotten password.',
+      table: {
+        category: 'Properties',
+        type: { summary: 'string' },
+        defaultValue: { summary: '¿Olvidaste tu contraseña?' },
+      },
+    },
+    buttonLabel: {
+      name: 'Button label',
+      control: 'text',
+      description: 'Sets the label for the continue button.',
+      table: {
+        category: 'Properties',
+        type: { summary: 'string' },
+        defaultValue: { summary: 'Ingresar' },
+      },
+    },
+    onRequest: {
+      name: 'On Request',
+      control: null,
+      description:
+        'Emits an event when a request is made, typically when the continue button is clicked.',
+      table: {
+        type: { summary: 'function' },
+        category: 'Events',
+      },
+    },
+    onContinue: {
+      name: 'On Continue',
+      control: null,
+      description:
+        'Emits an event when the continue action is completed successfully.',
+      table: {
+        type: { summary: 'function' },
+        category: 'Events',
+      },
+    },
+  },
+  args: {
+    headerLabel: 'ESTUDIANTES',
+    forgottenPasswordLabel: '¿Olvidaste tu contraseña?',
+    buttonLabel: 'Ingresar',
+  },
+} as Meta<typeof BmbLoginComponent>;
+
+type Story = StoryObj<BmbLoginComponent>;
+
+export const Default: Story = {};
