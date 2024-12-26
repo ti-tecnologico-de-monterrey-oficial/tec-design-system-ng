b1e0037fb0cb113698d99ce536ed2fcbe83c8345
María Guadalupe Ascencio Escamilla <t-marascencio@itesm.mx>
2024-12-26 11:05:14 -0600

    update: component change for micro grade version

--- projects/ds-ng/src/lib/components/bmb-profile/bmb-profile.component.spec.ts
+++ projects/ds-ng/src/lib/components/bmb-profile/bmb-profile.component.spec.ts
@@ -1,10 +1,12 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { BmbProfileComponent } from './bmb-profile.component';
+import { ComponentRef } from '@angular/core';
 
 describe('BmbProfileComponent', () => {
   let component: BmbProfileComponent;
   let fixture: ComponentFixture<BmbProfileComponent>;
+    let componentRef: ComponentRef<BmbProfileComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -12,7 +14,16 @@ describe('BmbProfileComponent', () => {
     }).compileComponents();
 
     fixture = TestBed.createComponent(BmbProfileComponent);
-    component = fixture.componentInstance;
+    component = fixture.componentInstance;componentRef = fixture.componentRef;
+    componentRef.setInput('userData', {
+      name: '',
+      userImg: '',
+      matricula: '',
+      mail:  '',
+      period:  '',
+      campus:  '',
+      program:  '',
+    });
     fixture.detectChanges();
   });
 
