2850661892fafcf5ffe4ce7194f3f14dd01a1901
María Guadalupe Ascencio Escamilla <t-marascencio@itesm.mx>
2025-01-13 11:30:43 -0600

    feat:Code refactoring, headers for title and subtitles, test

--- projects/ds-ng/src/lib/components/bmb-profile/bmb-profile.component.spec.ts
+++ projects/ds-ng/src/lib/components/bmb-profile/bmb-profile.component.spec.ts
@@ -1,10 +1,12 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { BmbProfileComponent } from './bmb-profile.component';
+import { ComponentRef } from '@angular/core';
 
 describe('BmbProfileComponent', () => {
   let component: BmbProfileComponent;
   let fixture: ComponentFixture<BmbProfileComponent>;
+    let componentRef: ComponentRef<BmbProfileComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -13,6 +15,16 @@ describe('BmbProfileComponent', () => {
 
     fixture = TestBed.createComponent(BmbProfileComponent);
     component = fixture.componentInstance;
+    componentRef = fixture.componentRef;
+    componentRef.setInput('userData', {
+      name: 'Juanito Perez',
+      userImg: 'https://picsum.photos/200/300',
+      matricula: 'A032132',
+      mail: 'mail@tec.mx',
+      period: 'AGO-DIC 24',
+      campus: 'Monterrey',
+      program: 'ARQ19',
+    });
     fixture.detectChanges();
   });
 
