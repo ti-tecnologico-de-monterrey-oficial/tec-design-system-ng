8abdff3febaea951556a52467d9b03ad119203c3
María Guadalupe Ascencio Escamilla <t-marascencio@itesm.mx>
2025-01-23 21:26:47 -0600

    feat: Home mitec refactoring of Frequent app selector

--- projects/ds-ng/src/lib/types/index.ts
+++ projects/ds-ng/src/lib/types/index.ts
@@ -55,8 +55,6 @@ export interface IBmbApp {
   link?: string;
   target?: IBmbTargetLink;
   appearance: IBmbInteractiveIconAppearance;
-  horizontal?: boolean;
-  setButtonTemplate?: boolean;
 }
 
 export interface IBmbNameValuePair {
