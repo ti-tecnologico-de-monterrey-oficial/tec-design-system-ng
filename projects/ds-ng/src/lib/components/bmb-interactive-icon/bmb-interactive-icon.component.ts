861ddfdcb80ea01c09efdc633bca96ab840dfb73
Juan Pedro Sanchez <t-juansanchez@itesm.mx>
2025-01-29 10:51:06 -0600

    Added: new colors and variantions

--- projects/ds-ng/src/lib/components/bmb-interactive-icon/bmb-interactive-icon.component.ts
+++ projects/ds-ng/src/lib/components/bmb-interactive-icon/bmb-interactive-icon.component.ts
@@ -16,7 +16,27 @@ export type IBmbInteractiveIconAppearance =
   | 'green'
   | 'yellow'
   | 'purple'
-  | 'none';
+  | 'none'
+  | 'mitec_blue'
+  | 'mitec_red'
+  | 'mitec_green'
+  | 'mitec_orange'
+  | 'mitec_light_green'
+  | 'mitec_purple'
+  | 'creative_violet'
+  | 'creative_indigo'
+  | 'creative_emerald'
+  | 'creative_licorice'
+  | 'creative_darkteal'
+  | 'creative_peach'
+  | 'creative_sepia'
+  | 'creative_softred'
+  | 'creative_wattle'
+  | 'creative_shipcove'
+  | 'creative_plantation'
+  | 'creative_rum'
+  | 'creative_hibiscus'
+  | 'creative_ripelemon';
 
 export type IBmbInteractiveIconType = 'regular' | 'button' | 'app_drawer';
 
