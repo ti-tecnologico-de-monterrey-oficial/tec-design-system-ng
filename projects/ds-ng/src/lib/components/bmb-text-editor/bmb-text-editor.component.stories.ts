42de870127d84c15614b487de69852aa49bc99bc
Juan Pedro Sanchez <t-juansanchez@itesm.mx>
2025-01-30 18:00:08 -0600

    Feat: Added: text editor

---
+++ projects/ds-ng/src/lib/components/bmb-text-editor/bmb-text-editor.component.stories.ts
@@ -0,0 +1,45 @@
+import type { Meta, StoryObj } from '@storybook/angular';
+import { BmbTextEditorComponent } from './bmb-text-editor.component';
+
+export default {
+  title: 'Macro Componentes/Text editor',
+  component: BmbTextEditorComponent,
+  parameters: {
+    docs: {
+      description: {
+        component: `
+Below is an example of how you can use this component in TypeScript:
+
+\`\`\`typescript
+import { BmbTextEditorComponent } from '@ti-tecnologico-de-monterrey-oficial/ds-ng';
+@Component({
+  selector: 'component',
+  standalone: true,
+  imports: [ BmbTextEditorComponent ],
+  templateUrl: './component.html',
+  styleUrl: './component.scss',
+})
+\`\`\`
+
+Below is an example of how you can use this component in HTML:
+        `,
+      },
+    },
+  },
+  argTypes: {
+    control: {
+      control: null,
+      description: 'Instance of FormControl to manage the input control state.',
+      table: {
+        category: 'Properties',
+        type: { summary: 'FormControl' },
+        defaultValue: { summary: "FormControl('', Validators.required)" },
+      },
+    },
+  },
+  args: {},
+} as Meta<typeof BmbTextEditorComponent>;
+
+type Story = StoryObj<BmbTextEditorComponent>;
+
+export const Default: Story = {};
