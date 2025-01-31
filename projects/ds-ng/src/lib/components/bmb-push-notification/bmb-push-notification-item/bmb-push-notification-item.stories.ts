78a322f91c25a885a95848ce85e150c8a5e95527
Juan Pedro Sanchez <t-juansanchez@itesm.mx>
2025-01-31 11:46:08 -0600

    Feat: Added new colors for push notifications

---
+++ projects/ds-ng/src/lib/components/bmb-push-notification/bmb-push-notification-item/bmb-push-notification-item.stories.ts
@@ -0,0 +1,86 @@
+import { Meta, StoryObj, moduleMetadata } from '@storybook/angular';
+import { BmbPushNotificationItemComponent } from './bmb-push-notification-item.component';
+
+export default {
+  title: 'Internal/Notification item',
+  component: BmbPushNotificationItemComponent,
+  decorators: [
+    moduleMetadata({
+      imports: [ BmbPushNotificationItemComponent ],
+    }),
+  ],
+  parameters: {
+    docs: {
+      description: {
+        component: `
+Below is an example of how you can use this component in TypeScript:
+
+\`\`\`typescript
+import { Component, ChangeDetectorRef } from '@angular/core';
+import {
+  FormControl,
+  FormBuilder,
+  FormGroup,
+  Validators,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { BmbPushNotificationItemComponent, BmbButtonDirective } from '@ti-tecnologico-de-monterrey-oficial/ds-ng';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-component',
+  standalone: true,
+  imports: [
+    ReactiveFormsModule,
+    BmbButtonDirective,
+    BmbPushNotificationItemComponent,
+  ],
+  templateUrl: './component.html',
+  styleUrls: ['./component.scss'],
+})
+\`\`\`
+        `,
+      },
+    },
+  },
+  argTypes: {
+    notification: {
+      name: 'Notification',
+      control: {
+        type: 'object',
+      },
+      description: 'The notification to display.',
+      table: {
+        category: 'Properties',
+        type: {
+          summary: 'INotification',
+          required: true
+        },
+      }
+    },
+    onClose: {
+      name: 'On Close',
+      control: null,
+      description: 'Event emitted when the notification is closed.',
+      table: {
+        category: 'Events',
+        type: {
+          summary: 'void',
+        },
+      }
+    }
+  },
+
+  args: {
+    notification: {
+      title: 'Notification title',
+      message: 'Notification message',
+      type: 'creative_rum',
+      isFullColor: false,
+    },
+  },
+} as Meta<typeof BmbPushNotificationItemComponent>;
+
+type Story = StoryObj<BmbPushNotificationItemComponent>;
+
+export const Default: Story = {};
