55870ded7700cce988bd9f6e8c662603c01ebaa2
María Guadalupe Ascencio Escamilla <t-marascencio@itesm.mx>
2025-02-21 23:43:14 -0600

    feat: New vertical layout directive and new icon status component

--- projects/ds-ng/src/lib/components/bmb-icon-status/bmp-icon.staus.stories.ts
+++ projects/ds-ng/src/lib/components/bmb-icon-status/bmp-icon.staus.stories.ts
@@ -2,7 +2,7 @@ import { Meta, StoryObj } from '@storybook/angular';
 import { BmbIconStatusComponent } from './bmb-icon-status.component';
 
 export default {
-  title: 'Macro Componentes/Icon status',
+  title: 'Micro Componentes/Icon status',
   component: BmbIconStatusComponent,
   decorators: [],
   parameters: {
