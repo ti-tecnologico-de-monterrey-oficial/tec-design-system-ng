86f83dad2459b65a43c5c9b437c04c75a5746bb2
Hazel Humberto Avila Soto <t-hazelavila@itesm.mx>
2025-02-25 09:50:37 -0600

    fix: Se arreglan test

--- projects/ds-ng/src/lib/components/bmb-digital-id/bmb-digital-id.component.spec.ts
+++ projects/ds-ng/src/lib/components/bmb-digital-id/bmb-digital-id.component.spec.ts
@@ -1,14 +1,24 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ComponentRef } from '@angular/core';
 import { BmbDigitalIdComponent } from './bmb-digital-id.component';
 
 describe('BmbDigitalIdComponent', () => {
   let component: BmbDigitalIdComponent;
   let fixture: ComponentFixture<BmbDigitalIdComponent>;
+  let componentRef: ComponentRef<BmbDigitalIdComponent>;
 
-  beforeEach(() => {
+  beforeEach(async () => {
     fixture = TestBed.createComponent(BmbDigitalIdComponent);
     component = fixture.componentInstance;
+    componentRef = fixture.componentRef;
+    componentRef.setInput('name', 'Paola');
+    componentRef.setInput('surname', 'Mendez');
+    componentRef.setInput('registration', 'A0323822');
+    componentRef.setInput('campus', 'Campus Norte');
+    componentRef.setInput('career', 'ITICS');
+    componentRef.setInput('role', 'Estudiante');
+    componentRef.setInput('imgProfile', 'https://farm2.staticflickr.com/1919/45579541712_f58c1fd0ed_o.jpg');
+    componentRef.setInput('imgBackground', 'https://2.bp.blogspot.com/-YkNDZEbKt_g/TYzcbF2_tkI/AAAAAAAAalk/Vt_MHS60Xv8/s1600/www.JoseLuisAvilaHerrera.BLOGSPOT.com%2B-%2BFunny%2BCats%2B-%2BGatitos%2Bmuy%2Btiernos%2B8.jpg');
     fixture.detectChanges();
   });
 
@@ -16,28 +26,9 @@ describe('BmbDigitalIdComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  describe('BmbDigitalIdComponent', () => {
-    let component: BmbDigitalIdComponent;
-    let fixture: ComponentFixture<BmbDigitalIdComponent>;
-
-    beforeEach(() => {
-      TestBed.configureTestingModule({
-        declarations: [BmbDigitalIdComponent],
-      }).compileComponents();
-
-      fixture = TestBed.createComponent(BmbDigitalIdComponent);
-      component = fixture.componentInstance;
-      fixture.detectChanges();
-    });
-
-    it('should create', () => {
-      expect(component).toBeTruthy();
-    });
-
-    it('should emit close event when closeDigitalId is called', () => {
-      spyOn(component.close, 'emit');
-      component.closeDigitalId();
-      expect(component.close.emit).toHaveBeenCalled();
-    });
+  it('should emit close event when closeDigitalId is called', () => {
+    spyOn(component.close, 'emit');
+    component.closeDigitalId();
+    expect(component.close.emit).toHaveBeenCalled();
   });
 });
--- projects/ds-ng/src/lib/components/bmb-home-card-chat/bmb-home-card-chat.component.spec.ts
+++ projects/ds-ng/src/lib/components/bmb-home-card-chat/bmb-home-card-chat.component.spec.ts
@@ -15,7 +15,16 @@ describe('BmbHomeCardChatComponent', () => {
     fixture = TestBed.createComponent(BmbHomeCardChatComponent);
     component = fixture.componentInstance;
     componentRef = fixture.componentRef;
-    componentRef.setInput('title', 'Test title');
+    componentRef.setInput('messagesHistory', 
+      [
+        {      
+          type: 'text',
+          content: { text: 'Hola, ¿cómo estás? En que puedo ayudarte' },
+          isUserMessage: false,
+          time: new Date('2025-02-19T14:31:00'),
+        },
+      ]
+    );
     fixture.detectChanges();
   });
 
