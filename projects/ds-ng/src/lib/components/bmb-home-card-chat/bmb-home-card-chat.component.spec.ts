192afd955fbbc506c5fad2c52069de63d2893c63
María Guadalupe Ascencio Escamilla <t-marascencio@itesm.mx>
2025-02-26 11:37:06 -0600

    merge into feature prod

--- projects/ds-ng/src/lib/components/bmb-home-card-chat/bmb-home-card-chat.component.spec.ts
+++ projects/ds-ng/src/lib/components/bmb-home-card-chat/bmb-home-card-chat.component.spec.ts
@@ -15,16 +15,14 @@ describe('BmbHomeCardChatComponent', () => {
     fixture = TestBed.createComponent(BmbHomeCardChatComponent);
     component = fixture.componentInstance;
     componentRef = fixture.componentRef;
-    componentRef.setInput('messagesHistory', 
-      [
-        {      
-          type: 'text',
-          content: { text: 'Hola, ¿cómo estás? En que puedo ayudarte' },
-          isUserMessage: false,
-          time: new Date('2025-02-19T14:31:00'),
-        },
-      ]
-    );
+    componentRef.setInput('messagesHistory', [
+      {
+        type: 'text',
+        content: { text: 'Hola, ¿cómo estás? En que puedo ayudarte' },
+        isUserMessage: false,
+        time: new Date('2025-02-19T14:31:00'),
+      },
+    ]);
     fixture.detectChanges();
   });
 
