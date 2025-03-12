4036c0a48f8ac1ffeedf520b0821a36d6c65383a
Hazel Humberto Avila Soto <t-hazelavila@itesm.mx>
2025-03-11 20:47:28 -0600

    feat(component/buttonGroup): Clase de estado activo

--- projects/ds-ng/src/lib/directives/bmb-button-group/bmb-button-group.stories.ts
+++ projects/ds-ng/src/lib/directives/bmb-button-group/bmb-button-group.stories.ts
@@ -66,7 +66,13 @@ Below is an example of how you can use this component in HTML:
 const customizable = (): StoryFn => (args) => ({
   props: args,
   template: `<section bmbButtonGroup [appearance]="appearance" [size]="size">
-    <button>Button 1</button>
+    <!-- IMPORTANT
+    You can add the class bmb_btn-toggle-active to set a button as active and keep that state of active, 
+    otherwise the button will just behave as a simple button without maintaining the activated state.
+    The styles of the buttons group are defined by Bamboo and will be adjusted according to the appearance
+    of the component.
+    -->
+    <button class="bmb_btn-toggle-active">Button 1</button>
     <button>Button 2</button>
     <button>Button 3</button>
   </section>`,
