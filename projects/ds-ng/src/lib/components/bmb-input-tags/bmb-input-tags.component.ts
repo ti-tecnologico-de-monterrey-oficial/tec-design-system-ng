e356fbd16aac4127aa91a97f4b53b14346e11407
Juan Pedro Sánchez Miranda <t-juansanchez@itesm.mx>
2025-05-02 14:08:56 -0600

    fix(component / input): onKeyDown - se mejoro la compatibilidad con este evento

--- projects/ds-ng/src/lib/components/bmb-input-tags/bmb-input-tags.component.ts
+++ projects/ds-ng/src/lib/components/bmb-input-tags/bmb-input-tags.component.ts
@@ -7,6 +7,7 @@ import {
   input,
   ViewEncapsulation,
   OnInit,
+  output,
 } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { BmbTooltipComponent } from '../bmb-tooltip/bmb-tooltip.component';
@@ -46,8 +47,10 @@ export class BmbInputTagsComponent implements OnInit {
   disabled = input<boolean>(false);
   maxSelectedItems = input<number>();
   name = input<string>(window.crypto.randomUUID());
-
   showError = input<boolean>(false);
+
+  onKeyDown = output<KeyboardEvent>();
+  onChange = output<string[]>();
 
   showDropdown: boolean = false;
   shouldShowError: boolean = false;
@@ -144,6 +147,18 @@ export class BmbInputTagsComponent implements OnInit {
     this.selectedTags.push(item);
     const selectedTagsString = this.selectedTags.map((tag) => tag.value);
     this.control().setValue(selectedTagsString);
+    this.onChange.emit(selectedTagsString);
+    this.filterControl.setValue('');
+  }
+
+  handleItemKeyDown(event: KeyboardEvent, item: IBmbDropdownItem) {
+    console.log('handleItemKeyDown', event, item);
+    if (event.key === 'Enter' || event.key === ' ') {
+      event.preventDefault();
+      this.handleItemClick(item);
+      this.filterControl.setValue('');
+      this.showDropdown = false;
+    }
   }
 
   removeTag(tag: IBmbDropdownItem) {
@@ -154,5 +169,23 @@ export class BmbInputTagsComponent implements OnInit {
 
   checkIfIsSelected(item: IBmbDropdownItem): boolean {
     return !this.selectedTags.some((tag) => tag.value === item.value);
+  }
+
+  handleKeyDown(event: KeyboardEvent) {
+    if (event.key === 'Enter') {
+      event.preventDefault();
+      const value = this.filterControl.value;
+      if (value && this.checkIfIsSelected({ name: value, value })) {
+        const newTag = { name: value, value };
+        this.handleItemClick(newTag);
+        this.filterControl.setValue('');
+        this.showDropdown = false;
+      }
+    }
+    // if (event.key === 'Backspace') {
+    //   if (this.selectedTags.length > 0) {
+    //     this.removeTag(this.selectedTags[this.selectedTags.length - 1]);
+    //   }
+    // }
   }
 }
