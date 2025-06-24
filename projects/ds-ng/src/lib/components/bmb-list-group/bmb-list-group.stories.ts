e1bfe126d02c1bec66784016697ae5806415db09
María Guadalupe Ascencio Escamilla <t-marascencio@itesm.mx>
2025-06-23 19:32:28 -0600

    Update bmb-list-group.stories.ts

--- projects/ds-ng/src/lib/components/bmb-list-group/bmb-list-group.stories.ts
+++ projects/ds-ng/src/lib/components/bmb-list-group/bmb-list-group.stories.ts
@@ -7,39 +7,6 @@ import { moduleMetadata } from '@storybook/angular';
 import { CommonModule } from '@angular/common';
 import { attributes } from '../../utils/utils';
 import { storiesLayoutHorizontal } from '../../utils/bambooLayout';
-import { IBbmBgAppearance } from '../bmb-advertisement-card/types';
-
-const appearanceOptions: IBbmBgAppearance[] = [
-  'normal',
-  'strong',
-  'success',
-  'info',
-  'warning',
-  'error',
-  'brand',
-  'background',
-  'disabled',
-  'mitec_blue',
-  'mitec_red',
-  'mitec_green',
-  'mitec_orange',
-  'mitec_light_green',
-  'mitec_purple',
-  'creative_violet',
-  'creative_indigo',
-  'creative_emerald',
-  'creative_licorice',
-  'creative_darkteal',
-  'creative_peach',
-  'creative_sepia',
-  'creative_softred',
-  'creative_wattle',
-  'creative_shipcove',
-  'creative_plantation',
-  'creative_rum',
-  'creative_hibiscus',
-  'creative_ripelemon',
-];
 
 export default {
   title: 'Components/Containers/List group',
