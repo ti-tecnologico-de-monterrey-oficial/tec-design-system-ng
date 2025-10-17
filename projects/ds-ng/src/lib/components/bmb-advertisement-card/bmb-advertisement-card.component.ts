9dd59a9d83c087136709785b295a6ed7952e9033
Juan Sánchez <t-juansanchez@itesm.mx>
2025-10-17 13:35:35 -0600

    feat(component / advertisement card): Se agregó el servicio de traducción para este componente

--- projects/ds-ng/src/lib/components/bmb-advertisement-card/bmb-advertisement-card.component.ts
+++ projects/ds-ng/src/lib/components/bmb-advertisement-card/bmb-advertisement-card.component.ts
@@ -11,6 +11,8 @@ import { IBmbAdvertisementData } from './types';
 import { BmbHomeCardComponent } from '../bmb-home-card/bmb-home-card.component';
 import { BmbCarouselComponent } from '../bmb-carousel/bmb-carousel.component';
 import { BmbTabsComponent, IBmbTab } from '../bmb-tabs/bmb-tabs.component';
+import es from '../../../assets/i18n/es.json';
+import { TranslatePipe } from '../../pipes/translations';
 
 @Component({
   selector: 'bmb-advertisement-card',
@@ -21,6 +23,7 @@ import { BmbTabsComponent, IBmbTab } from '../bmb-tabs/bmb-tabs.component';
     BmbHomeCardComponent,
     BmbCarouselComponent,
     BmbTabsComponent,
+    TranslatePipe,
   ],
   templateUrl: './bmb-advertisement-card.component.html',
   styleUrl: './bmb-advertisement-card.component.scss',
@@ -29,8 +32,8 @@ import { BmbTabsComponent, IBmbTab } from '../bmb-tabs/bmb-tabs.component';
 })
 export class BmbAdvertisementCardComponent {
   data = model<IBmbAdvertisementData>();
-  title = input<string>('Mis Anuncios');
-  subtitle = input<string>('');
+  title = input<string>();
+  subtitle = input<string>();
 
   expanded: boolean = false;
   selectedTabId: number = 0;
