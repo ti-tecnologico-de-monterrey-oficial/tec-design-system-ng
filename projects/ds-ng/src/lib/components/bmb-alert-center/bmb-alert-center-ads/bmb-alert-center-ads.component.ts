2298e0b3ff1fdde51efbe78798713db407c927f8
Juan Sánchez <t-juansanchez@itesm.mx>
2025-10-29 13:58:56 -0600

    feat(component / alert center): Se agregó la posibilidad de incrustar contenido de HTML a los anuncios

--- projects/ds-ng/src/lib/components/bmb-alert-center/bmb-alert-center-ads/bmb-alert-center-ads.component.ts
+++ projects/ds-ng/src/lib/components/bmb-alert-center/bmb-alert-center-ads/bmb-alert-center-ads.component.ts
@@ -5,6 +5,7 @@ import {
   output,
   ViewEncapsulation,
 } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 import { IBmbAlertEmptyState, IBmbDataAlert } from '../types';
 import { CommonModule } from '@angular/common';
 import { BmbImageComponent } from '../../bmb-image/bmb-image.component';
@@ -12,6 +13,7 @@ import { BmbButtonDirective } from '../../../directives/bmb-button/button.direct
 import { BmbCarouselComponent } from '../../bmb-carousel/bmb-carousel.component';
 import { BmbLayoutItemDirective } from '../../../directives/bmb-layout/bmb-layout-item.directive';
 import { BmbAlertCenterEmptyComponent } from '../bmb-alert-center-empty/bmb-alert-center-empty.component';
+import DOMPurify from 'dompurify';
 
 @Component({
   selector: 'bmb-alert-center-ads',
@@ -42,7 +44,30 @@ export class BmbAlertCenterAdsComponent {
 
   alertEvent = output<IBmbDataAlert>();
 
+  constructor(private sanitizer: DomSanitizer) {}
+
   handleAlertEvent(alert: IBmbDataAlert) {
     this.alertEvent.emit(alert);
+  }
+
+  sanitizedHtml(html: string) {
+    const clean = DOMPurify.sanitize(html, {
+      FORBID_TAGS: ['script', 'style', 'iframe', 'object', 'embed'],
+      FORBID_ATTR: [
+        'style',
+        'onerror',
+        'onclick',
+        'onkeyup',
+        'onload',
+        'onmouseover',
+        'onfocus',
+        'onkeydown',
+        'onchange',
+        'onblur',
+        'onsubmit',
+      ],
+    });
+
+    return this.sanitizer.bypassSecurityTrustHtml(clean);
   }
 }
--- projects/ds-ng/src/lib/components/bmb-alert-center/types.ts
+++ projects/ds-ng/src/lib/components/bmb-alert-center/types.ts
@@ -11,7 +11,7 @@ export interface IBmbDataAlertDetails {
   text: string;
   style?: 'normal' | 'bold' | 'italic' | 'underline';
   href?: string;
-  type: 'paragraph' | 'image' | 'video' | 'link' | 'button' | 'title';
+  type: 'paragraph' | 'image' | 'video' | 'link' | 'button' | 'title' | 'html';
   variant?: IButtonAppearance;
   event?: (id: string | number) => void;
 }
