3b2cf557cea8de6786afca863cc10b49792c10c6
María Guadalupe Ascencio Escamilla <t-marascencio@itesm.mx>
2025-11-18 14:17:14 -0600

    core: depuración de herramientas de mostradas en Storybook.

--- projects/ds-ng/.storybook/manager.ts
+++ projects/ds-ng/.storybook/manager.ts
@@ -3,6 +3,10 @@ import bambooDarkTheme from './bambooDarkTheme';
 import { API_PreparedIndexEntry } from 'storybook/internal/types';
 
 addons.setConfig({
+  toolbar: {
+    'storybook/background': { hidden: true },
+  },
+
   theme: bambooDarkTheme,
   sidebar: {
     filters: {
