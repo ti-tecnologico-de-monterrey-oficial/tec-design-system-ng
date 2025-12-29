83a63b2a734e5a1c93e4c8e266da7bd177bef81f
Juan Sánchez <t-juansanchez@itesm.mx>
2025-12-29 17:16:16 -0600

    Update main.ts

--- projects/ds-ng/.storybook/main.ts
+++ projects/ds-ng/.storybook/main.ts
@@ -14,8 +14,8 @@ const config: StorybookConfig = {
     options: {},
   },
   docs: {
-    // docsMode: process.env.STORYBOOK_DOCS_MODE === 'true',
-    docsMode: true,
+    docsMode: process.env.STORYBOOK_DOCS_MODE === 'true',
+    // docsMode: true,
     defaultName: 'Documentation',
   },
   webpackFinal: async (config) => {
