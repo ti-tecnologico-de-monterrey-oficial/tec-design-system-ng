a6b13720989b8b94ca005882684c20cf830f7e6f
María Guadalupe Ascencio Escamilla <t-marascencio@itesm.mx>
2025-12-31 09:39:29 -0600

    Update bambooDarkTheme.ts

--- projects/ds-ng/.storybook/bambooDarkTheme.ts
+++ projects/ds-ng/.storybook/bambooDarkTheme.ts
@@ -11,15 +11,30 @@ export default create({
   brandImage: './logo.png',
   brandTarget: '_self',
 
+  // Colors
   colorPrimary: '#F6F7F9',
   colorSecondary: '#2766CB',
+
+  // UI
   appBg: '#1F222E',
   appContentBg: '#313649',
   appPreviewBg: '#F6F7F9',
   appBorderColor: '#3F4965',
+  // appBorderRadius: 4,
+
+  // Text colors
   textColor: '#F6F7F9',
+  // textInverseColor: '#ffffff',
+
+  // Toolbar default and active colors
   barTextColor: '#F6F7F9',
   barSelectedColor: '#fff',
   barHoverColor: '#fff',
   barBg: '#313649',
+
+  // Form colors
+  // inputBg: '#ffffff',
+  // inputBorder: '#10162F',
+  // inputTextColor: '#10162F',
+  // inputBorderRadius: 2,
 });
