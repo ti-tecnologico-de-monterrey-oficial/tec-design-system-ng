0087a5204e96656b240d4803dd5fc6aede27ae5f
María Guadalupe Ascencio Escamilla <t-marascencio@itesm.mx>
2026-01-08 14:01:04 -0600

    chore: se usaron colores de 'Bamboo' principalmente de contenedor para la apariencia de 'Storybook' y las historias.

--- projects/ds-ng/.storybook/bambooDarkTheme.ts
+++ projects/ds-ng/.storybook/bambooDarkTheme.ts
@@ -1,40 +1,41 @@
 import { create } from 'storybook/theming';
 
 export default create({
+  //Theme
   base: 'dark',
 
+  // Typography
   fontBase: '"Poppins-Regular", sans-serif',
   fontCode: 'monospace',
 
+  //Brand
   brandTitle: 'Bamboo Design System',
   brandUrl: 'https://bamboo.tec.mx/',
   brandImage: './logo.png',
   brandTarget: '_self',
 
-  // Colors
-  colorPrimary: '#F6F7F9',
+  // Sidebar text colors
+  colorPrimary: '#EBEDF3',
   colorSecondary: '#2766CB',
 
   // UI
-  appBg: '#1F222E',
-  appContentBg: '#313649',
-  appPreviewBg: '#F6F7F9',
-  appBorderColor: '#3F4965',
-  // appBorderRadius: 4,
+  appBg: '#313649',
+  appContentBg: '#373F55',
+  appPreviewBg: '#D3D7E4',
+  appBorderColor: '#617196',
+  appBorderRadius: 4,
+
+  //Button colors
+  buttonBorder: '#DEDEDE',
 
   // Text colors
   textColor: '#F6F7F9',
-  // textInverseColor: '#ffffff',
+  textInverseColor: '#1F222E',
+  textMutedColor: '#ADB6CC',
 
-  // Toolbar default and active colors
-  barTextColor: '#F6F7F9',
-  barSelectedColor: '#fff',
-  barHoverColor: '#fff',
-  barBg: '#313649',
-
-  // Form colors
-  // inputBg: '#ffffff',
-  // inputBorder: '#10162F',
-  // inputTextColor: '#10162F',
-  // inputBorderRadius: 2,
+  // Toolbar colors
+  barTextColor: '#EBEDF3',
+  barSelectedColor: '#ADB6CC',
+  barHoverColor: '#617196',
+  barBg: '#1F222E',
 });
