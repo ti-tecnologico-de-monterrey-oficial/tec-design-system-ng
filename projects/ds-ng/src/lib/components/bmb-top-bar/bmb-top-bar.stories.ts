01135a59b8b410b3b03f25297bb509580d3e1085
María Guadalupe Ascencio Escamilla <t-marascencio@itesm.mx>
2026-01-27 15:13:53 -0600

    chore: se realizó depuración de controles mostrados en la documentación de Strorybook.

--- projects/ds-ng/src/lib/components/bmb-top-bar/bmb-top-bar.stories.ts
+++ projects/ds-ng/src/lib/components/bmb-top-bar/bmb-top-bar.stories.ts
@@ -48,6 +48,8 @@ export default {
           'mobileImageMitecDefault',
           'showAnimation',
           'showUserName',
+          'getNoMobileResolutionSize',
+          'handleSearchChange'
         ],
       },
       description: {
