5e01b394065908412365f69379f41ac83d46dbdd
María Guadalupe Ascencio Escamilla <t-marascencio@itesm.mx>
2026-03-04 22:59:40 -0600

    feat(component/container-button): se agregó variante grades según diseño en Figma. Se implementaron solo las entradas necesarias.

---
+++ projects/ds-ng/src/lib/components/bmb-container-button/bmb-container-button-grade/bmb-container-button-grade.component.ts
@@ -0,0 +1,34 @@
+import { CommonModule } from '@angular/common';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  input,
+  ViewEncapsulation,
+} from '@angular/core';
+import { BmbContainerButtonBaseComponent } from '../bmb-container-button-base/bmb-container-button-base.component';
+import { BmbIconComponent } from '../../bmb-icon/bmb-icon.component';
+import { BmbTitleComponent } from '../../bmb-title/bmb-title.component';
+import { BmbGradeValueComponent } from '../../bmb-grade-value/bmb-grade-value.component';
+
+@Component({
+  selector: 'bmb-container-button-grade',
+  standalone: true,
+  imports: [
+    CommonModule,
+    BmbContainerButtonBaseComponent,
+    BmbGradeValueComponent,
+    BmbTitleComponent,
+    BmbIconComponent,
+  ],
+  templateUrl: './bmb-container-button-grade.component.html',
+  styleUrl: './bmb-container-button-grade.component.scss',
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class BmbContainerButtonGradeComponent {
+  componentTitle = input.required<string>();
+  subtitle = input<string>('');
+  score = input.required<number | string>();
+  isDisable = input<boolean>(false);
+  isError = input<boolean>(false);
+}
