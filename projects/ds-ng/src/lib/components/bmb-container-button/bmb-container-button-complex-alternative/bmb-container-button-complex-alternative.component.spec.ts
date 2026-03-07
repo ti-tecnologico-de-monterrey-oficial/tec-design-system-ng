ec782d6ca58435ab887fb4a37ff94d1712f5dd41
María Guadalupe Ascencio Escamilla <t-marascencio@itesm.mx>
2026-03-06 18:32:29 -0600

    feat(component/container-button): se agregó variante 'complex alternative' según diseño en Figma. Se implementaron solo las entradas necesarias.

---
+++ projects/ds-ng/src/lib/components/bmb-container-button/bmb-container-button-complex-alternative/bmb-container-button-complex-alternative.component.spec.ts
@@ -0,0 +1,29 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { BmbContainerButtonComplexAlternativeComponent } from './bmb-container-button-complex-alternative.component';
+import { ComponentRef } from '@angular/core';
+
+describe('BmbContainerButtonComplexAlternativeComponent', () => {
+  let component: BmbContainerButtonComplexAlternativeComponent;
+  let fixture: ComponentFixture<BmbContainerButtonComplexAlternativeComponent>;
+let componentRef: ComponentRef<BmbContainerButtonComplexAlternativeComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [BmbContainerButtonComplexAlternativeComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(
+      BmbContainerButtonComplexAlternativeComponent,
+    );
+    component = fixture.componentInstance;
+    componentRef = fixture.componentRef;
+    componentRef.setInput('componentTitle', 'Test');
+    componentRef.setInput('leftIconName', 'Test');
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
