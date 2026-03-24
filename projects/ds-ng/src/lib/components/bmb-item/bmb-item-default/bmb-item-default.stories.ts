47a50b27ae758119e8bcbf86f118588243c711fc
María Guadalupe Ascencio Escamilla <t-marascencio@itesm.mx>
2026-03-24 15:00:53 -0600

    feat(component/action-menu): se implemento componente para los elementos de la variante default.

---
+++ projects/ds-ng/src/lib/components/bmb-item/bmb-item-default/bmb-item-default.stories.ts
@@ -0,0 +1,45 @@
+import { Meta, StoryObj, componentWrapperDecorator } from '@storybook/angular';
+import { getActionMenuItemComponentDescription } from '../../../utils/doc/utils';
+import {
+  DBmbIconParamDesc,
+  DBmbItemActionMenu,
+} from '../../../utils/doc/parameterDescriptions';
+import { BmbItemDefaultComponent } from './bmb-item-default.component';
+
+export default {
+  title: 'Dev tools/Item/Default',
+  component: BmbItemDefaultComponent,
+  tags: ['!autodocs'],
+  decorators: [
+    componentWrapperDecorator((story: string) => {
+      return `<div style="max-width: 560px; margin: 0 auto">
+        ${story}
+      </div>`;
+    }),
+  ],
+  parameters: {
+    docs: {
+      description: {
+        component: `
+${getActionMenuItemComponentDescription({
+  selectorName: 'default',
+  variantName: 'default',
+  componentName: 'BmbItemDefaultComponent',
+})}
+  `,
+      },
+    },
+  },
+  argTypes: {
+    icon: DBmbIconParamDesc.icon,
+    label: DBmbItemActionMenu.label,
+  },
+  args: {
+    label: 'Text',
+    icon: 'face',
+  },
+} as Meta<typeof BmbItemDefaultComponent>;
+
+type Story = StoryObj<BmbItemDefaultComponent>;
+
+export const Default: Story = {};
