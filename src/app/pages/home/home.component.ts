5471c6848b96905cf889f8c13bf8988190aa8174
María Guadalupe Ascencio Escamilla <t-marascencio@itesm.mx>
2026-05-23 12:53:13 -0600

    chore: escenario de error de bmb-icon

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,7 +2,9 @@ import {
   Component,
   computed,
   effect,
+  Input,
   model,
+  SimpleChanges,
   TemplateRef,
   ViewChild,
   ViewEncapsulation,
@@ -65,6 +67,9 @@ import { ModalWDropdownComponent } from '../../components/modal-w-dropdown/modal
   encapsulation: ViewEncapsulation.None,
 })
 export class HomeComponent {
+  @Input() id_grado_logro!: number | string | null | undefined;
+  @Input() nombre_grado_logro: string = '';
+
   bookmarkActive = model<boolean>(false);
   @ViewChild('modalTemplate') modalTemplate!: TemplateRef<unknown>;
   @ViewChild('notificationTemplate')
@@ -146,5 +151,65 @@ export class HomeComponent {
 
   printPaymentDetails(event: MouseEvent | KeyboardEvent): void {
     console.log('Payment details printed', event);
+  }
+
+  private readonly configLogros: Record<
+    string | number,
+    { claseColor: string; llenos: number }
+  > = {
+    0: { claseColor: '--general-contrasts-80', llenos: 0 }, //No hay suficiente información
+    1: { claseColor: '--creative-use-peach', llenos: 1 },
+    2: { claseColor: '--semantic-success', llenos: 2 },
+    3: { claseColor: '--semantic-success', llenos: 3 },
+    4: { claseColor: '--semantic-success', llenos: 4 },
+    5: { claseColor: '--creative-use-peach', llenos: 1 },
+    6: { claseColor: '--semantic-success', llenos: 2 },
+    7: { claseColor: '--semantic-success', llenos: 3 },
+    8: { claseColor: '--semantic-success', llenos: 4 },
+    PENDIENTE: { claseColor: '--general-contrasts-80', llenos: 0 },
+  };
+
+  claseColorActual: string = '--general-contrasts-80';
+  barrasLlenas: number = 0;
+  arrBarras: number[] = [1, 2, 3, 4];
+
+  ngOnInit(): void {
+    this.actualizarConfiguracion();
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['id_grado_logro']) {
+      this.actualizarConfiguracion();
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.barrasLlenas = 0;
+    this.claseColorActual = '--general-contrasts-80';
+  }
+
+  protected actualizarConfiguracion(): void {
+    // Si llega un valor vacío o nulo, lo tratamos como PENDIENTE (0 barras)
+    const idBuscar =
+      this.id_grado_logro === null ||
+      this.id_grado_logro === undefined ||
+      this.id_grado_logro === ''
+        ? 'PENDIENTE'
+        : this.id_grado_logro;
+
+    const config = this.configLogros[idBuscar];
+
+    if (config) {
+      this.claseColorActual = config.claseColor;
+      this.barrasLlenas = config.llenos;
+    } else {
+      // Forzamos reseteo si el ID no es válido
+      this.claseColorActual = '--general-contrasts-80';
+      this.barrasLlenas = 0;
+    }
+  }
+
+  public fillIcon(index: number): boolean {
+    return index <= this.barrasLlenas;
   }
 }
