8cd5e3a0723ad089d37d0a3641bd514b06d2d1b6
María Guadalupe Ascencio Escamilla <t-marascencio@itesm.mx>
2026-05-27 16:39:48 -0600

    chore: cambio en la especificación del uso del tipos de colores descontinuados.

--- projects/ds-ng/src/lib/components/bmb-badge/bmb-badge.stories.ts
+++ projects/ds-ng/src/lib/components/bmb-badge/bmb-badge.stories.ts
@@ -42,7 +42,7 @@ ${getArchitectureSection(`<section class="bmb_badge"> <!-- conditional classes b
 ${getSpecialSpecifications(
   `
 ${getAlertBlockquote(
-  `Follow name colors are deprecated:
+  `Follow name colors are LTS:
  - 'strong',
  - 'success',
  - 'info',
@@ -72,8 +72,8 @@ ${getAlertBlockquote(
  - 'creative_ripelemon',
 <br/><br/>Please do not use them because they will be removed in future versions.`,
   {
-    title: '###'.concat(RELEVANT_TITLE.deprecated),
-    blockquoteType: BlockquoteType.warning,
+    title: '###'.concat(RELEVANT_TITLE.lts),
+    blockquoteType: BlockquoteType.important,
     isRelevantTitle: true,
   },
 )}
