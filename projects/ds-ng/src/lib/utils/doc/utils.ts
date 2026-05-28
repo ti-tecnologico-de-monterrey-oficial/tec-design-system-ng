ebce3485def3227e12edc1ec0083d1ef022ea8a7
María Guadalupe Ascencio Escamilla <t-marascencio@itesm.mx>
2026-05-27 18:05:25 -0600

    se descarta el cambio

--- projects/ds-ng/src/lib/utils/doc/utils.ts
+++ projects/ds-ng/src/lib/utils/doc/utils.ts
@@ -41,7 +41,6 @@ export const RELEVANT_TITLE = {
   configuration: '⚙️**Configuration**<br/>',
   example: '⭐**Example**<br/>',
   deprecated: '⛔**Deprecated**<br/>',
-  lts: '**LTS (Long-Term Support)',
 };
 
 export const BlockquoteType = {
