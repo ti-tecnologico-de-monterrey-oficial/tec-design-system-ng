0ea73ab8da8a3fc6f861206ad8c9f5a3bb294ca7
Juan Sánchez <t-juansanchez@itesm.mx>
2026-06-01 13:40:05 -0600

    old removed

--- old/docker-build.sh
+++
@@ -1,1 +0,0 @@
-docker build -t angular-lib-builder  -f Dockerfile-17 .
--- old/projects/ds-ng/src/lib/utils/currencyFormat.ts
+++
@@ -1,6 +0,0 @@
-export const currencyFormat = (amount: number): string => {
-  return Intl.NumberFormat('es-MX', {
-    style: 'currency',
-    currency: 'MXN',
-  }).format(amount);
-};
