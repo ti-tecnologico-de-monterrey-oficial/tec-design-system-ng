a09f4295a420ee66e212ea765d69200e821eafc2
Jesus Alcala <t-jesalcala@itesm.mx>
2026-06-12 10:24:25 -0600

    Merge branch 'nx-bamboo' of https://github.com/ti-tecnologico-de-monterrey-oficial/tec-design-system-ng into nx-bamboo

---
+++ lib/ui-angular/src/lib/components/bmb-divider/bmb-divider.component.ts
@@ -0,0 +1,33 @@
+import {
+  Component,
+  ChangeDetectionStrategy,
+  ViewEncapsulation,
+  input,
+} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+  getDividerClasses,
+  type BmbDividerType,
+} from '@ti-tecnologico-de-monterrey-oficial/core/component/divider';
+
+
+@Component({
+  selector: 'bmb-divider',
+  styleUrl: './bmb-divider.component.scss',
+  templateUrl: './bmb-divider.component.html',
+  standalone: true,
+  imports: [CommonModule],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  encapsulation: ViewEncapsulation.None,
+})
+export class BmbDividerComponent {
+  type = input<BmbDividerType>('simple');
+  removeMargin = input<boolean>(false);
+
+  getClasses(): string[] {
+    return getDividerClasses({
+      type: this.type(),
+      removeMargin: this.removeMargin(),
+    });
+  }
+}
