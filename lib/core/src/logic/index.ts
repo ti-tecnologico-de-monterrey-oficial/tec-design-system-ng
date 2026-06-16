dfdc228493069d9d7a38eef193065ae24af74012
Jesus Alcala <t-jesalcala@itesm.mx>
2026-06-16 15:58:44 -0600

    chore: container migrado a rama nx

--- lib/core/src/logic/index.ts
+++ lib/core/src/logic/index.ts
@@ -1,3 +1,4 @@
 export * from './components/badge/badge.js';
 export * from './components/card/card.js';
 export * from './components/divider/divider.js';
+export * from './components/container/container.js';
---
+++ lib/ui-angular/src/lib/components/bmb-container/bmb-container.component.ts
@@ -0,0 +1,33 @@
+import {
+  Component,
+  ChangeDetectionStrategy,
+  ViewEncapsulation,
+  input,
+} from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import {
+  getContainerClasses,
+  type IBmbAppearanceType,
+} from '@ti-tecnologico-de-monterrey-oficial/core/component/container';
+
+@Component({
+  selector: 'bmb-container',
+  standalone: true,
+  imports: [CommonModule],
+  styleUrl: './bmb-container.component.scss',
+  templateUrl: './bmb-container.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  encapsulation: ViewEncapsulation.None,
+})
+export class BmbContainerComponent {
+  appearance = input<IBmbAppearanceType>('primary-container');
+  isHidden = input<boolean>(false);
+
+  getClasses(): string[] {
+    return getContainerClasses({
+      appearance: this.appearance(),
+      isHidden: this.isHidden(),
+    });
+  }
+}
