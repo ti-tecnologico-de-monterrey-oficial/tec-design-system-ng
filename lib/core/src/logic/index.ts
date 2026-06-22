7e9137de2974ab83f06306da4f6dd25f705e7bc1
Jesus Alcala <t-jesalcala@itesm.mx>
2026-06-22 16:20:57 -0600

    chore: legend desacoplado

--- lib/core/src/logic/index.ts
+++ lib/core/src/logic/index.ts
@@ -3,3 +3,4 @@ export * from './components/card/card.js';
 export * from './components/divider/divider.js';
 export * from './components/container/container.js';
 export * from './components/title/title.js';
+export * from './components/legend/legend.js';
---
+++ lib/ui-angular/src/lib/components/bmb-legend/bmb-legend.component.ts
@@ -0,0 +1,40 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  input,
+  ViewEncapsulation,
+} from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import {
+  getLegendBulletClass,
+  getLegendValueClass,
+  type IBmbLegendVariations,
+} from '@ti-tecnologico-de-monterrey-oficial/core/component/legend';
+
+@Component({
+  selector: 'bmb-legend',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './bmb-legend.component.html',
+  styleUrl: './bmb-legend.component.scss',
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class BmbLegendComponent {
+  label = input<string>('');
+  value = input<string>('');
+  indicatorAppearance = input<IBmbLegendVariations>('normal');
+  isOperationState = input<boolean>(false);
+
+  getBulletClass(): string {
+    return getLegendBulletClass(this.indicatorAppearance());
+  }
+
+  getValueClass(): string {
+    return getLegendValueClass(
+      this.indicatorAppearance(),
+      this.isOperationState(),
+    );
+  }
+}
