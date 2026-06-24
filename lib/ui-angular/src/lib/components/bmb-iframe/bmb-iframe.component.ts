1660c47694e1a164c943e227895752f03e8dcb4d
Jesus Alcala <t-jesalcala@itesm.mx>
2026-06-24 17:07:37 -0600

    chore: iframe desacoplado

---
+++ lib/ui-angular/src/lib/components/bmb-iframe/bmb-iframe.component.ts
@@ -0,0 +1,26 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  input,
+  ViewEncapsulation,
+} from '@angular/core';
+import { BmbIframePipeTransform } from './bmb-iframe.pipe';
+
+import { type BmbIframeReferrerPolicy } from '@ti-tecnologico-de-monterrey-oficial/core/component/iframe';
+
+@Component({
+  selector: 'bmb-iframe',
+  standalone: true,
+  imports: [BmbIframePipeTransform],
+  templateUrl: './bmb-iframe.component.html',
+  styleUrl: './bmb-iframe.component.scss',
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class BmbIframeComponent {
+  height = input<string | number>('100%');
+  src = input.required<string>();
+  width = input<string | number>('100%');
+  loading = input<'eager' | 'lazy'>('eager');
+  name = input<string>('');
+}
