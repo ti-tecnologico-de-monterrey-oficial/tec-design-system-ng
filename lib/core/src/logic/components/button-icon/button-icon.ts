2045d5776ae9f1796d8d421712cc6c46dcd31376
Jesus Alcala <t-jesalcala@itesm.mx>
2026-07-07 17:31:33 -0600

    chore: breadcrumb y button-icon desacoplados

---
+++ lib/core/src/logic/components/button-icon/button-icon.ts
@@ -0,0 +1,29 @@
+import { IBmbContrast } from '../../../types/colors';
+
+export interface IBmbButtonIconClassesProps {
+  active: boolean;
+  showContainer: boolean;
+  disabled: boolean;
+  isOutline: boolean;
+  appearanceContrast: IBmbContrast;
+}
+
+export const getButtonIconClasses = ({
+  active,
+  showContainer,
+  disabled,
+  isOutline,
+  appearanceContrast,
+}: IBmbButtonIconClassesProps): Record<string, boolean> => ({
+  'bmb_button_icon-active': active,
+  'bmb_button_icon-container': showContainer,
+  'bmb_button_icon-container-outline':
+    showContainer && isOutline,
+  'bmb_button_icon-disabled': disabled,
+  'bmb_button_icon-container-primary':
+    appearanceContrast === 'primary',
+  'bmb_button_icon-container-alternative':
+    appearanceContrast === 'alternative',
+  'bmb_button_icon-container-solid':
+    appearanceContrast === 'solid',
+});
--- lib/core/src/logic/index.ts
+++ lib/core/src/logic/index.ts
@@ -17,3 +17,5 @@ export * from './components/balance-overview/balance-overview.js';
 export * from './components/bookmark/bookmark.js';
 export * from './components/box-icon/box-icon.js';
 export * from './components/icon/icon.js';
+export * from './components/breadcrumb/breadcrumb.js';
+export * from './components/button-icon/button-icon.js';
