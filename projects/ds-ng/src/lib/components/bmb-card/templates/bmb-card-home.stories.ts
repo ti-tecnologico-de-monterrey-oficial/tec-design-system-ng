3404c04a6e7d86050cff1d86881517b727ac7ca3
EmmKallTec <t-emmcalderon@itesm.mx>
2026-08-01 21:32:11 -0600

    feat(template) home desktop/responsive

--- projects/ds-ng/src/lib/components/bmb-card/templates/bmb-card-home.stories.ts
+++ projects/ds-ng/src/lib/components/bmb-card/templates/bmb-card-home.stories.ts
@@ -1,17 +1,61 @@
 import { Meta, moduleMetadata } from '@storybook/angular';
-import { BmbCardComponent, BmbCardContentComponent, BmbCardHeaderComponent } from '../bmb-card.component';
-import { BmbBadgeComponent } from '../../bmb-badge/bmb-badge.component';
+import { BmbCardComponent } from '../bmb-card.component';
+import { BmbCheckboxComponent } from '../../bmb-checkbox/bmb-checkbox.component';
+import { BmbIconComponent } from '../../bmb-icon/bmb-icon.component';
 import { BmbListGroupComponent } from '../../bmb-list-group/bmb-list-group.component';
 import { BmbListGroupItemComponent } from '../../bmb-list-group/bmb-list-group-item/bmb-list-group-item.component';
-import { BmbLayoutDirective } from '../../../directives/bmb-layout/bmb-layout.directive';
-import { BmbLayoutItemDirective } from '../../../directives/bmb-layout/bmb-layout-item.directive';
-import { BmbVerticalLayoutDirective } from '../../../directives/bmb-layout/bmb-vertical-layout/bmb-vertical-layout.directive';
-import { BmbVerticalLayoutItemDirective } from '../../../directives/bmb-layout/bmb-vertical-layout/bmb-vertical-layout-item.directive';
 import { staticCardStory } from './bmb-card-template-story.utils';
 
-const meta: Meta<BmbCardComponent> = { title: 'Components/Containers/Generic card/Home', component: BmbCardComponent, tags: ['!autodocs'], decorators: [moduleMetadata({ imports: [BmbCardComponent, BmbCardHeaderComponent, BmbCardContentComponent, BmbBadgeComponent, BmbListGroupComponent, BmbListGroupItemComponent, BmbLayoutDirective, BmbLayoutItemDirective, BmbVerticalLayoutDirective, BmbVerticalLayoutItemDirective] })] };
+const meta: Meta<BmbCardComponent> = {
+  title: 'Components/Containers/Generic card/Home',
+  component: BmbCardComponent,
+  tags: ['!autodocs'],
+  decorators: [
+    moduleMetadata({
+      imports: [
+        BmbCardComponent,
+        BmbCheckboxComponent,
+        BmbIconComponent,
+        BmbListGroupComponent,
+        BmbListGroupItemComponent,
+      ],
+    }),
+  ],
+};
 export default meta;
-const item = (id: string) => `<bmb-list-group-item id="${id}" [personalizedTemplate]="true"><div class="bmb-card-template__list-item" bmbLayout margin="none" gapSize="m" alignItems="center" justify="spaceBetween" [avoidRowWrap]="true"><div bmbLayoutItem [isDynamicItem]="true" [colGrow]="1"><h4>Lorem ipsum dolor sit amet, consectetur</h4><p>&bull; Subtitle</p></div><bmb-badge bmbLayoutItem text="Badge" appearance="blue-light" /></div></bmb-list-group-item>`;
-const template = (mobile = false) => `<div class="bmb-card-template bmb-card-template--home${mobile ? ' bmb-card-template--mobile' : ''}"><bmb-card type="normal" borderRadius="xl" margin="none" boxShadowStyle="box-shadow-3"><div bmbVerticalLayout layoutHeight="590px" margin="none" gapSize="none"><bmb-card-header bmbVerticalLayoutItem padding="none"><h3 class="bmb-card-template__header">Title</h3></bmb-card-header><bmb-card-content bmbVerticalLayoutItem [rowGrow]="1" padding="none"><div class="bmb-card-template__scroll" bmbVerticalLayout margin="none" gapSize="m"><div bmbVerticalLayoutItem class="bmb-card-template__summary" bmbLayout margin="none" justify="spaceBetween" alignItems="center" [avoidRowWrap]="true"><span bmbLayoutItem>Lorem ipsum</span><span bmbLayoutItem>0 / 00</span></div><bmb-list-group bmbVerticalLayoutItem margin="m" padding="none">${item('home-1')}${item('home-2')}${item('home-3')}${item('home-4')}</bmb-list-group></div></bmb-card-content></div></bmb-card></div>`;
+
+const item = (id: string, checked: boolean) => `
+  <bmb-list-group-item id="${id}" [personalizedTemplate]="true">
+    <div class="bmb-card-template__home-row">
+      <bmb-checkbox ariaLabel="Select file" [checked]="${checked}" />
+      <div class="bmb-card-template__home-file">
+        <bmb-icon icon="image" [size]="24" alt="Document image" />
+        <div class="bmb-card-template__home-file-copy">
+          <h4>Nombre_Archivo.doc</h4>
+          <p>Descripción del documento agregado</p>
+        </div>
+        <bmb-icon icon="file_open" [size]="24" alt="Open file" />
+      </div>
+    </div>
+  </bmb-list-group-item>`;
+
+const template = (mobile = false) => `
+  <div class="bmb-card-template bmb-card-template--home${mobile ? ' bmb-card-template--mobile' : ''}">
+    <bmb-card type="normal" borderRadius="xl" margin="none">
+      <div class="bmb-card-template__home-surface">
+        <h3>Title</h3>
+        <p class="bmb-card-template__home-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce volutpat rhoncus leo vel pharetra. Donec feugiat enim</p>
+        <hr />
+        <div class="bmb-card-template__home-scroll">
+          <bmb-list-group padding="none" margin="none">
+            ${item('home-file-1', true)}
+            ${item('home-file-2', false)}
+            ${item('home-file-3', true)}
+          </bmb-list-group>
+        </div>
+      </div>
+    </bmb-card>
+  </div>`;
+
 export const Desktop = staticCardStory(template());
 export const Mobile = staticCardStory(template(true), true);
