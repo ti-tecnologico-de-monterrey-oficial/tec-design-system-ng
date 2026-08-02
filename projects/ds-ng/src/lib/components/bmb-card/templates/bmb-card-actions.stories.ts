deb8857b35e10fab62fdf596c1348b08501d0245
EmmKallTec <t-emmcalderon@itesm.mx>
2026-08-01 21:16:19 -0600

    feat(template) action2 desktop/responsive

--- projects/ds-ng/src/lib/components/bmb-card/templates/bmb-card-actions.stories.ts
+++ projects/ds-ng/src/lib/components/bmb-card/templates/bmb-card-actions.stories.ts
@@ -2,14 +2,31 @@ import { Meta, moduleMetadata } from '@storybook/angular';
 import { BmbCardComponent } from '../bmb-card.component';
 import { BmbBoxIconComponent } from '../../bmb-box-icon/bmb-box-icon.component';
 import { BmbButtonDirective } from '../../../directives/bmb-button/button.directive';
-import { BmbLayoutDirective } from '../../../directives/bmb-layout/bmb-layout.directive';
-import { BmbLayoutItemDirective } from '../../../directives/bmb-layout/bmb-layout-item.directive';
-import { BmbVerticalLayoutDirective } from '../../../directives/bmb-layout/bmb-vertical-layout/bmb-vertical-layout.directive';
-import { BmbVerticalLayoutItemDirective } from '../../../directives/bmb-layout/bmb-vertical-layout/bmb-vertical-layout-item.directive';
 import { staticCardStory } from './bmb-card-template-story.utils';
 
-const meta: Meta<BmbCardComponent> = { title: 'Components/Containers/Generic card/Actions', component: BmbCardComponent, tags: ['!autodocs'], decorators: [moduleMetadata({ imports: [BmbCardComponent, BmbBoxIconComponent, BmbButtonDirective, BmbLayoutDirective, BmbLayoutItemDirective, BmbVerticalLayoutDirective, BmbVerticalLayoutItemDirective] })] };
+const meta: Meta<BmbCardComponent> = {
+  title: 'Components/Containers/Generic card/Actions',
+  component: BmbCardComponent,
+  tags: ['!autodocs'],
+  decorators: [
+    moduleMetadata({
+      imports: [BmbCardComponent, BmbBoxIconComponent, BmbButtonDirective],
+    }),
+  ],
+};
 export default meta;
-const template = (mobile = false) => `<div class="bmb-card-template bmb-card-template--actions${mobile ? ' bmb-card-template--mobile' : ''}"><bmb-card type="normal" borderRadius="m" margin="none"><div class="bmb-card-template__surface" ${mobile ? 'bmbLayout margin="none" gapSize="m" alignItems="center" [avoidRowWrap]="true"' : 'bmbVerticalLayout margin="none" gapSize="m" justify="center" alignItems="center"'}><bmb-box-icon ${mobile ? 'bmbLayoutItem' : 'bmbVerticalLayoutItem'} iconName="crop_16_9" boxShape="circle" boxColor="black-primary" /><div class="bmb-card-template__copy" ${mobile ? 'bmbLayoutItem [isDynamicItem]="true" [colGrow]="1"' : 'bmbVerticalLayoutItem'}><h3>Title</h3><p>Text<br />content</p></div><button bmbButton ${mobile ? 'bmbLayoutItem' : 'bmbVerticalLayoutItem'} appearance="secondary-outlined">Button</button></div></bmb-card></div>`;
+const template = (mobile = false) =>
+  `<div class="bmb-card-template bmb-card-template--actions${mobile ? ' bmb-card-template--mobile' : ''}">
+    <bmb-card type="normal" borderRadius="m" margin="none">
+      <div class="bmb-card-template__surface">
+        <bmb-box-icon class="bmb-card-template__icon" iconName="home" boxSize="small" boxShape="circle" boxColor="black-primary" />
+        <div class="bmb-card-template__copy">
+          <h3>Title</h3>
+          <p>Text<br />content</p>
+        </div>
+        <button bmbButton appearance="secondary-outlined">Button</button>
+      </div>
+    </bmb-card>
+  </div>`;
 export const Desktop = staticCardStory(template());
 export const Mobile = staticCardStory(template(true), true);
