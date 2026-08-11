ae830ee516a0df904d665f32a6fc446f9d5a3274
Jesus Alcala <t-jesalcala@itesm.mx>
2026-08-10 22:10:02 -0600

    update icon

---
+++ ui-angular/src/lib/_shared/types/components/bmb-custom-icons.ts
@@ -0,0 +1,22 @@
+﻿export type BmbCustomIconListType =
+  | 'bmb_android'
+  | 'bmb_apple'
+  | 'bmb_drive'
+  | 'bmb_facebook'
+  | 'bmb_instagram'
+  | 'bmb_teams'
+  | 'bmb_twitter'
+  | 'bmb_whatsapp'
+  | 'bmb_youtube';
+
+export const BmbCustomIconList: BmbCustomIconListType[] = [
+  'bmb_android',
+  'bmb_apple',
+  'bmb_drive',
+  'bmb_facebook',
+  'bmb_instagram',
+  'bmb_teams',
+  'bmb_twitter',
+  'bmb_whatsapp',
+  'bmb_youtube',
+];
