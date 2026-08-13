4b5d7b7519a2b8848b4bc2273e7d7c5e7f0dfc15
Carlos Adrián Solares Lozano <carlos.solares@tec.mx>
2026-08-12 18:22:09 -0600

    chore: connect dropdown top bar and user image

---
+++ code-connect-batch/Dropdown.figma.ts
@@ -0,0 +1,32 @@
+// url=https://www.figma.com/design/Q4t8qIM5fklC9I3Atc1BrZ/Bamboo-Design-System---Components?node-id=109-35160
+// source=projects/ds-ng/src/lib/components/bmb-dropdown/bmb-dropdown.component.ts
+// component=BmbDropdownComponent
+import figma from 'figma'
+
+const instance = figma.selectedInstance
+const isFilterable = instance.getEnum('Variant', {
+  Default: 'false',
+  'Search input': 'true',
+  'Multi select': 'false',
+})
+const isMultiSelect = instance.getEnum('Variant', {
+  Default: 'false',
+  'Search input': 'false',
+  'Multi select': 'true',
+})
+const disabled = instance.getEnum('State', {
+  'Focused - Active': 'false',
+  Disabled: 'true',
+  Hover: 'false',
+  Enabled: 'false',
+  Error: 'false',
+  'Enabled - Filled': 'false',
+})
+
+export default {
+  example: figma.code`<bmb-dropdown [isFilterable]="${isFilterable}" [isMultiSelect]="${isMultiSelect}" [disabled]="${disabled}" />`,
+  imports: [
+    "import { BmbDropdownComponent } from '@ti-tecnologico-de-monterrey-oficial/ds-ng'",
+  ],
+  id: 'bmb-dropdown',
+}
