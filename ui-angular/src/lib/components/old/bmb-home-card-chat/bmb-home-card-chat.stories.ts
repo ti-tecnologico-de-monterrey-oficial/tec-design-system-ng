fc331ed7bfae41b11f95e1b2a37c30711b6cd484
María Guadalupe Ascencio Escamilla <t-marascencio@itesm.mx>
2026-08-18 16:21:41 -0600

    chore: se agregaron más íconos al listado.

--- ui-angular/src/lib/components/old/bmb-home-card-chat/bmb-home-card-chat.stories.ts
+++ ui-angular/src/lib/components/old/bmb-home-card-chat/bmb-home-card-chat.stories.ts
@@ -195,12 +195,28 @@ ${getBasicExampleBlock(
   botList: IBotType[] = [
     {
       name: 'TecBot',
+      label: 'Tecbot Standard',
       icon: 'bot_tecStandar',
     },
+    { name: 'ChatGPT', label: 'ChatGPT', icon: 'bot_chatGPT' },
     {
-      name: 'SupportBot',
-      icon: 'support_agent',
+      name: 'TecGPT',
+      label: 'TecGPT',
+      icon: 'bot_tecGPT',
     },
+    { name: 'Comment', label: 'Comment', icon: 'comment' },
+    { name: 'New', label: 'New', icon: 'new' },
+    { name: 'BotSchool', label: 'Tecbot school', icon: 'bot_tecSchool' },
+    { name: 'BotTech', label: 'Tecbot tech', icon: 'bot_tecTech' },
+    { name: 'BotSport', label: 'Tecbot sport', icon: 'bot_tecSport' },
+    { name: 'BotPhone', label: 'Tecbot phone', icon: 'bot_tecPhone' },
+    { name: 'BotMedic', label: 'Tecbot healt', icon: 'bot_health' },
+    { name: 'BotScience', label: 'Tecbot science', icon: 'bot_tecScience' },
+    { name: 'Empty', label: 'Empty', icon: 'empty' },
+    { name: 'Anthropic', label: 'Anthropic', icon: 'anthropic' },
+    { name: 'Meta', label: 'Meta', icon: 'meta' },
+    { name: 'Xai', label: 'Xai', icon: 'xai' },
+    { name: 'Google', label: 'Google', icon: 'google' },
   ];
 
   messages: IBmbChatMessage[] = [
@@ -633,8 +649,28 @@ handleNewChat() {
     botList: [
       {
         name: 'TecBot',
+        label: 'Tecbot Standard',
         icon: 'bot_tecStandar',
       },
+      { name: 'ChatGPT', label: 'ChatGPT', icon: 'bot_chatGPT' },
+      {
+        name: 'TecGPT',
+        label: 'TecGPT',
+        icon: 'bot_tecGPT',
+      },
+      { name: 'Comment', label: 'Comment', icon: 'comment' },
+      { name: 'New', label: 'New', icon: 'new' },
+      { name: 'BotSchool', label: 'Tecbot school', icon: 'bot_tecSchool' },
+      { name: 'BotTech', label: 'Tecbot tech', icon: 'bot_tecTech' },
+      { name: 'BotSport', label: 'Tecbot sport', icon: 'bot_tecSport' },
+      { name: 'BotPhone', label: 'Tecbot phone', icon: 'bot_tecPhone' },
+      { name: 'BotMedic', label: 'Tecbot healt', icon: 'bot_health' },
+      { name: 'BotScience', label: 'Tecbot science', icon: 'bot_tecScience' },
+      { name: 'Empty', label: 'Empty', icon: 'empty' },
+      { name: 'Anthropic', label: 'Anthropic', icon: 'anthropic' },
+      { name: 'Meta', label: 'Meta', icon: 'meta' },
+      { name: 'Xai', label: 'Xai', icon: 'xai' },
+      { name: 'Google', label: 'Google', icon: 'google' },
     ],
 
     messages: [
