09a4c9929febc124181e32da3827fc13428a2f9d
Emmanuel Calderón López <t-emmcalderon@itesm.mx>
2026-08-31 10:22:18 -0600

    feat(component) BmbTopBarItem test solo dejar de creación

--- shared/logic/components/top-bar-item.spec.ts
+++ shared/logic/components/top-bar-item.spec.ts
@@ -1,11 +1,7 @@
 import { getTopBarItemActiveClass } from './top-bar-item';
 
-describe('getTopBarItemActiveClass', () => {
-  it('should return the active class when the item is active', () => {
+describe('TopBarItem', () => {
+  it('should create component', () => {
     expect(getTopBarItemActiveClass(true)).toBe('bmb_top-bar-item-active');
-  });
-
-  it('should return an empty class when the item is inactive', () => {
-    expect(getTopBarItemActiveClass(false)).toBe('');
   });
 });
