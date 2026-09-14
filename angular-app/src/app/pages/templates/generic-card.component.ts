680596c1bb37b99fcf26ca75c92e7b2aa8d26062
Emmanuel Calderón López <t-emmcalderon@itesm.mx>
2026-09-14 12:42:48 -0600

    feat(template) templates en html

--- angular-app/src/app/pages/templates/generic-card.component.ts
+++ angular-app/src/app/pages/templates/generic-card.component.ts
@@ -1,4 +1,7 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { map } from 'rxjs';
 import { BmbTitleComponent } from '../../../../../ui-angular/src/lib/components/bmb-title/bmb-title.component';
 import {
   BmbBadgeComponent,
@@ -7,6 +10,8 @@ import {
   BmbCardComponent,
   BmbCardContentComponent,
   BmbCardHeaderComponent,
+  BmbDividerComponent,
+  BmbFocusElementComponent,
   BmbIconComponent,
   BmbImageComponent,
   BmbLayoutDirective,
@@ -26,6 +31,8 @@ import {
     BmbCardComponent,
     BmbCardContentComponent,
     BmbCardHeaderComponent,
+    BmbDividerComponent,
+    BmbFocusElementComponent,
     BmbIconComponent,
     BmbImageComponent,
     BmbLayoutDirective,
@@ -40,6 +47,22 @@ import {
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class GenericCard {
+  readonly isItemListMobile = toSignal(
+    inject(BreakpointObserver)
+      .observe('(width < 1001px)')
+      .pipe(map((state) => state.matches)),
+    { initialValue: false },
+  );
+
+  readonly informativeItems = [
+    { id: 1, stackedMobileActions: true, showSubtitle: true, showComplement: true },
+    { id: 2, stackedMobileActions: true, showSubtitle: true, showComplement: true },
+    { id: 3, stackedMobileActions: false, showSubtitle: true, showComplement: true },
+    { id: 4, stackedMobileActions: false, showSubtitle: true, showComplement: true },
+    { id: 5, stackedMobileActions: false, showSubtitle: true, showComplement: true },
+    { id: 6, stackedMobileActions: false, showSubtitle: false, showComplement: false },
+  ];
+
   readonly informativeImage =
     'https://conecta.tec.mx/sites/default/files/inline-images/tec-de-monterrey.webp';
 
